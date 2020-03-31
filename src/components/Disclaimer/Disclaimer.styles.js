5024c700d66d462592cd2d5620e47cf8fa6d9a7b
Ben McAlindin <bmcalindin@asidatascience.com>
2020-03-31 18:39:28 +0100

    Update disclaimer

--- src/components/Disclaimer/Disclaimer.styles.js
+++ src/components/Disclaimer/Disclaimer.styles.js
@@ -6,6 +6,6 @@ import type { ComponentType } from 'react';
 export const Container: ComponentType<*> = (() => {
   return styled.p`
     grid-column: 1/-1;
-    font-size: 1rem;
+    font-size: 0.75rem;
   `;
 })();
