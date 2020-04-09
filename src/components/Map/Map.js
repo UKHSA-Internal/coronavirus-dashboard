f1a33bcd29a6997274abacd1bc22bf57fa1fcb87
Ben McAlindin <bmcalindin@asidatascience.com>
2020-04-09 15:07:08 +0100

    Use open streep maps

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -68,10 +68,8 @@ const Map: ComponentType<Props> = ({
         center: [55, -4],
         zoom: 4.5, 
         layers: [
-          L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-            subdomains: 'abcd',
-            maxZoom: 20,
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           })
         ]
       });
