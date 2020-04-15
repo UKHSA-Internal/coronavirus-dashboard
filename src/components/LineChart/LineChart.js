ce6184706f23173a70be9a74050f0be327eaccef
Leon Brocard <acme@astray.com>
2020-04-15 07:32:29 +0100

    Localise number on Y axis on the charts
    All numbers on the page are localised, with the exception of numbers
    on the Y axis on the charts. Where I am, this means that numbers on
    the page are formatted with commas for thousands "93,873", but on
    the Y axis of the chart it has "80000". This commit localises the
    number of the Y axis, so it now shows "80,000".

--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -54,6 +54,12 @@ const LineChart: ComponentType<Props> = ({ header, tooltipText, data }: Props) =
                 gridLines: {
                   drawBorder: false,
                 },
+                ticks: {
+                  beginAtZero: true,
+                  userCallback: function(value, index, values) {
+                    return value.toLocaleString();
+                  },
+                },
               }],
             },
             tooltips: {
