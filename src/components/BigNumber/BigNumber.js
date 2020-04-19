a953b70368bc3f3bf539298d60921f6536885c00
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-04-19 16:23:10 +0100

    Additional description under BigNumber

--- src/components/BigNumber/BigNumber.js
+++ src/components/BigNumber/BigNumber.js
@@ -7,11 +7,13 @@ import numeral from 'numeral';
 import type { Props } from './BigNumber.types.js';
 import * as Styles from './BigNumber.styles.js';
 
-const BigNumber: ComponentType<Props> = ({ caption, number, percentageChange, subtext, asterisk }: Props) => {
+const BigNumber: ComponentType<Props> = ({ caption, number, description, percentageChange, subtext, asterisk }: Props) => {
   return (
     <Styles.Container>
-      <Styles.Caption className="govuk-caption-s">{caption}</Styles.Caption>
+      <Styles.Caption className="govuk-caption-l">{caption}</Styles.Caption>
       <Styles.Number className="govuk-heading-l">{numeral(number).format('0,0')}{asterisk ? '*' : ''}</Styles.Number>
+      <Styles.Caption className="govuk-!-font-size-16">{description}</Styles.Caption>
+
       {/* <Styles.PercentageChange className="govuk-body">
         {percentageChange}%&nbsp;
         <Styles.Triangle percentageChange={percentageChange} />
