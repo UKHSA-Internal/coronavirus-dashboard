dc34994f6e817fb53af3dbf1e47250b7fa8a0e3e
Ben McAlindin <bmcalindin@asidatascience.com>
2020-04-20 15:29:05 +0100

    Add google analytics

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,7 +3,9 @@
 import React from 'react';
 import type { ComponentType } from 'react';
 import { Footer } from 'govuk-react-jsx/govuk/components/footer';
+import { Helmet } from 'react-helmet';
 
+import CookieBanner from 'components/CookieBanner';
 import Header from 'components/Header';
 import Navigation from 'components/Navigation';
 
@@ -12,6 +14,17 @@ import type { Props } from './Layout.types.js';
 const Layout: ComponentType<Props> = ({ pathname, children }: Props) => {
   return (
     <>
+      <Helmet>
+        <script>
+          {`
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          `}
+        </script>
+      </Helmet>
+      <CookieBanner />
       <Header />
       <Navigation pathname={pathname} />
       {children}
