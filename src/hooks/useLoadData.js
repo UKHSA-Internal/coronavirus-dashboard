1eaa812bf4dde34062fadde1705deccdee8e1e21
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-04-23 16:45:56 +0100

    address the x-origin issue on chrome

--- src/hooks/useLoadData.js
+++ src/hooks/useLoadData.js
@@ -3,14 +3,14 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const latestDataUrl = 'https://coronavirus.data.gov.uk/downloads/data/data_latest.json';
+const latestDataUrl = 'https://c19downloads.azureedge.net/downloads/data/data_latest.json';
 
 const useLoadData = () => {
     const [data, setData] = useState<?Data>(null);
 
     useEffect(() => {
         const getData = async () => {
-            const {data: d} = await axios.get(latestDataUrl, {crossDomain: true});
+            const {data: d} = await axios.get(latestDataUrl);
             setData(d);
         };
 
