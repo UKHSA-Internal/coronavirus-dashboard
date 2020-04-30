c08f1c49301be67546639ed1ffa84ac47d2bd111
Mike Coats <mike.coats@nature.scot>
2020-04-30 15:47:54 +0100

    Change how React mounts into its DOM container
    Previously, the app mounted to the `#root` element. The meant that we
    couldn't also _really_ use it for back-to-top navigation as the fragment
    in the url name would look weird. Instead, this renames the containing
    element to `#top` for navigation purposes and we instruct React to mount
    via a class based querySelector on the new `.app-root` class.
    
    Issue: PublicHealthEngland/coronavirus-dashboard#97

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ ReactDOM.render(
     <Router>
       <App />
     </Router>,
-  document.getElementById('root'),
+  document.querySelector('.app-root'),
 );
 
 // If you want your app to work offline and load faster, you can change
