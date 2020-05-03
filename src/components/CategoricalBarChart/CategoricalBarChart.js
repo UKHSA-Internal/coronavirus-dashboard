74c7116fc2910434737dad6e317712e34601d71f
Pouria Hadjibagheri <xenatisch@users.noreply.github.com>
2020-05-03 13:10:30 +0100

    Responsive y-axis font size

--- src/components/CategoricalBarChart/CategoricalBarChart.js
+++ src/components/CategoricalBarChart/CategoricalBarChart.js
@@ -52,6 +52,8 @@ const getBarChartData = ({ data, categoryLabels, colors, columnLabelGetter }: Ca
 
 
 const getBarChartOptions = (tooltipText, mobileView: boolean) => {
+    
+    const labelFontSize = mobileView ? 11 : 14;
 
     return {
         barValueSpacing: 20,
@@ -69,7 +71,7 @@ const getBarChartOptions = (tooltipText, mobileView: boolean) => {
                 stacked: false,
                 ticks: {
                     minRotation: 45,
-                    fontSize: mobileView ? 11 : 14,
+                    fontSize: labelFontSize,
                     fontColor: '#1A2B2B',
                     autoSkip: false,
                     maxTicksLimit: 15,
