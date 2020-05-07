3b827201f6226d06e0b6108181f63c4489d97020
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-05-07 15:52:42 +0100

    Markdown integration for the disclaimer

--- src/components/Disclaimer/Disclaimer.js
+++ src/components/Disclaimer/Disclaimer.js
@@ -1,17 +1,59 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import type { ComponentType } from 'react';
+
+import slug from "remark-slug";
+import html from "remark-html";
+import remark from "remark";
+import externalLink from "remark-external-links";
 
 import type { Props } from './Disclaimer.types';
 import * as Styles from './Disclaimer.styles';
+import toc from "remark-toc";
 
-const Disclaimer: ComponentType<Props> = ({ text }: Props) => {
-  return (
-    <Styles.Container className="govuk-body-s">
-      {text}
-    </Styles.Container>
-  );
-};
+export default class Disclaimer extends Component<Props, {}> {
 
-export default Disclaimer;
+    state = {
+
+        data: null,
+        loading: false
+
+    } // state
+
+    processData  = () => {
+
+        const { text } = this.props;
+
+        remark()
+            .use(toc)
+            .use(slug)
+            .use(externalLink)
+            .use(html).process(text, (err, text) =>
+                this.setState({
+                    data: err ? text : String(text),
+                    loading: false
+                })
+        );
+
+    } // processData
+
+    componentDidMount(): * {
+
+        this.setState({ loading: true }, this.processData)
+
+    } // componentDidMount
+
+    render(): React$Node {
+
+        const { data, loading } = this.state;
+
+        if ( loading ) return null;
+
+        return <Styles.Container className={ "disclaimer" }>
+            <div className={ "markdown" } dangerouslySetInnerHTML={{ __html: data }}/>
+        </Styles.Container>
+
+    } // render
+
+}
