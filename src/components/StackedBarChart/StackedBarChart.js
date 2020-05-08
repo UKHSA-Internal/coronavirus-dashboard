3142f56f2b4f141be61629e13361c6ae0d68de91
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-05-08 12:12:22 +0100

    Adds moving average to bar charts

--- src/components/StackedBarChart/StackedBarChart.js
+++ src/components/StackedBarChart/StackedBarChart.js
@@ -3,7 +3,8 @@
 import React from 'react';
 import type { ComponentType } from 'react';
 import { Bar } from 'react-chartjs-2';
-import * as moment from 'moment';
+
+import { fillDateGaps, movingAverage } from "common/utils";
 
 import type { Props } from './StackedBarChart.types';
 import * as Styles from './StackedBarChart.styles';
@@ -27,20 +28,34 @@ const sortFunc = (a, b) => {
 
 const getBarChartData = ({ previous, change }) => {
 
-    const previousSorted = previous.sort(sortFunc)
+    const
+        previousSorted = fillDateGaps(previous),
+        changeData = fillDateGaps(change),
+        sum = previousSorted.map(({ value }, index) => value + changeData[index].value);
 
     return {
         labels: previousSorted.map(d => d.date),
         datasets: [
             {
+                label: "Moving average of the total",
+                data: movingAverage(sum, 7),
+                type: 'line',
+                fill: false,
+                borderColor: "#912b88",
+                borderWidth: 2,
+                order: 0
+            },
+            {
                 label: "Previously reported",
                 backgroundColor: '#367E93',
-                data: previousSorted.map(d => d.value)
+                data: previousSorted
+                    .map(({ value }) => value)
             },
             {
                 label: "Newly reported",
                 backgroundColor: '#0a495a',
-                data: change.sort(sortFunc).map(d => d.value > 0 ? d.value : 0)
+                data: changeData
+                    .map(({ value }) => value > 0 ? value : 0)
             }
         ]
     }
@@ -57,6 +72,11 @@ const getBarChartOptions = (tooltipText, mobileView) => {
         legend: {
             display: true,
             position: 'bottom'
+        },
+        elements: {
+            point:{
+                radius: 0
+            }
         },
         scales: {
             xAxes: [{
@@ -83,7 +103,7 @@ const getBarChartOptions = (tooltipText, mobileView) => {
                 gridLines: {
                     drawBorder: false,
                 },
-                stacked: true,
+                // stacked: true,
                 ticks: {
                     fontSize: labelFontSize,
                     fontColor: '#1A2B2B',
@@ -94,76 +114,76 @@ const getBarChartOptions = (tooltipText, mobileView) => {
                 },
             }],
         },
-        tooltips: {
-            // Disable the on-canvas tooltip
-            enabled: false,
-
-            custom: function (tooltipModel) {
-                // Tooltip Element
-                let tooltipEl = document.getElementById('chartjs-tooltip');
-
-                // Create element on first render
-                if (!tooltipEl) {
-                    tooltipEl = document.createElement('div');
-                    tooltipEl.id = 'chartjs-tooltip';
-                    tooltipEl.innerHTML = '<table style="background-color: #000; color: #fff; border-radius: 5px; padding: 2px; opacity: 0.8"></table>';
-                    document.body.appendChild(tooltipEl);
-                }
-
-                // Hide if no tooltip
-                if (tooltipModel.opacity === 0) {
-                    tooltipEl.style.opacity = 0;
-                    return;
-                }
-
-                function getBody(bodyItem) {
-                    return bodyItem.lines;
-                }
-
-                // Set Text
-                if (tooltipModel.body) {
-                    const titleLines = tooltipModel.title || [],
-                        bodyLines = tooltipModel.body.map(getBody);
-                    let innerHtml = '<thead>';
-                    titleLines.forEach(function (title) {
-                        innerHtml += '<tr><th class="govuk-body govuk-!-font-weight-bold govuk-!-margin-0" style="text-align: left; color: #fff; font-size: 14px;">'
-                            + new Intl.DateTimeFormat('en-GB', {
-                                year: 'numeric',
-                                month: '2-digit',
-                                day: '2-digit'
-                            }).format(new Date(title))
-                            + '</th></tr>';
-                    });
-                    innerHtml += '</thead><tbody>';
-
-                    bodyLines.forEach(function (body, i) {
-                        const
-                            val = body,
-                            number = numeral(/\d+/.exec(val)[0]).format('0,0'),
-                            text = /[^\d]+/.exec(val)[0].trim(),
-                            contentText = `${text} ${number}`,
-                            style = `border-width: 2px; color: #fff; font-size: 14px;`;
-
-                        innerHtml += `<tr><td><span class="govuk-body govuk-!-margin-0" style="${style}"/>${contentText} ${[tooltipText]}</td></tr>`;
-                    });
-                    innerHtml += '</tbody>';
-
-                    let tableRoot = tooltipEl.querySelector('table');
-                    tableRoot.innerHTML = innerHtml;
-                }
-
-                // `this` will be the overall tooltip
-                const position = this._chart.canvas.getBoundingClientRect();
-
-                // Display, position, and set styles for font
-                tooltipEl.style.opacity = 1;
-                tooltipEl.style.position = 'absolute';
-                tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
-                tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
-                tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
-                tooltipEl.style.pointerEvents = 'none';
-            }
-        }
+        // tooltips: {
+        //     // Disable the on-canvas tooltip
+        //     enabled: false,
+        //
+        //     custom: function (tooltipModel) {
+        //         // Tooltip Element
+        //         let tooltipEl = document.getElementById('chartjs-tooltip');
+        //
+        //         // Create element on first render
+        //         if (!tooltipEl) {
+        //             tooltipEl = document.createElement('div');
+        //             tooltipEl.id = 'chartjs-tooltip';
+        //             tooltipEl.innerHTML = '<table style="background-color: #000; color: #fff; border-radius: 5px; padding: 2px; opacity: 0.8"></table>';
+        //             document.body.appendChild(tooltipEl);
+        //         }
+        //
+        //         // Hide if no tooltip
+        //         if (tooltipModel.opacity === 0) {
+        //             tooltipEl.style.opacity = 0;
+        //             return;
+        //         }
+        //
+        //         function getBody(bodyItem) {
+        //             return bodyItem.lines;
+        //         }
+        //
+        //         // Set Text
+        //         if (tooltipModel.body) {
+        //             const titleLines = tooltipModel.title || [],
+        //                 bodyLines = tooltipModel.body.map(getBody);
+        //             let innerHtml = '<thead>';
+        //             titleLines.forEach(function (title) {
+        //                 innerHtml += '<tr><th class="govuk-body govuk-!-font-weight-bold govuk-!-margin-0" style="text-align: left; color: #fff; font-size: 14px;">'
+        //                     + new Intl.DateTimeFormat('en-GB', {
+        //                         year: 'numeric',
+        //                         month: '2-digit',
+        //                         day: '2-digit'
+        //                     }).format(new Date(title))
+        //                     + '</th></tr>';
+        //             });
+        //             innerHtml += '</thead><tbody>';
+        //
+        //             bodyLines.forEach(function (body, i) {
+        //                 const
+        //                     val = body,
+        //                     number = numeral(/\d+/.exec(val)[0]).format('0,0'),
+        //                     text = /[^\d]+/.exec(val)[0].trim(),
+        //                     contentText = `${text} ${number}`,
+        //                     style = `border-width: 2px; color: #fff; font-size: 14px;`;
+        //
+        //                 innerHtml += `<tr><td><span class="govuk-body govuk-!-margin-0" style="${style}"/>${contentText} ${[tooltipText]}</td></tr>`;
+        //             });
+        //             innerHtml += '</tbody>';
+        //
+        //             let tableRoot = tooltipEl.querySelector('table');
+        //             tableRoot.innerHTML = innerHtml;
+        //         }
+        //
+        //         // `this` will be the overall tooltip
+        //         const position = this._chart.canvas.getBoundingClientRect();
+        //
+        //         // Display, position, and set styles for font
+        //         tooltipEl.style.opacity = 1;
+        //         tooltipEl.style.position = 'absolute';
+        //         tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
+        //         tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+        //         tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
+        //         tooltipEl.style.pointerEvents = 'none';
+        //     }
+        // }
 
     }
 
