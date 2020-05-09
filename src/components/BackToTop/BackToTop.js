ad4bc28a433ecf0cb4e7e3343f61a22aba749f27
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-05-09 09:47:27 +0100

    Incorporates icons module into "back to top"

--- src/components/BackToTop/BackToTop.js
+++ src/components/BackToTop/BackToTop.js
@@ -3,8 +3,9 @@
 import React from 'react';
 import type { ComponentType } from 'react';
 
-import type { Props } from './BackToTop.types';
+import { ArrowUp } from "common/icons";
 import * as Styles from './BackToTop.styles';
+import type { Props } from './BackToTop.types';
 
 import useBackToTopOverlayVisible from 'hooks/useBackToTopOverlayVisible';
 
@@ -25,16 +26,7 @@ const BackToTopLink: ComponentType<Props> = () => {
                      role={ "button" }
                      href={ "" }
                      onClick={ backToTopClickHandler }>
-            <svg role={ "presentation" }
-                 focusable={ "false" }
-                 className={ "back-to-top" }
-                 xmlns={ "http://www.w3.org/2000/svg" }
-                 width={ "13" }
-                 height={ "17" }
-                 viewBox={ "0 0 13 17" }>
-                <path fill={ "currentColor" }
-                      d={ "M6.5 0L0 6.5 1.4 8l4-4v12.7h2V4l4.3 4L13 6.4z" }/>
-            </svg>
+            <ArrowUp/>
             Back to top
         </Styles.Link>
     </div>
