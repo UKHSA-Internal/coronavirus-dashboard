c5905c5a514db9e5f2bfc57ef759dc970b8dc131
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-05-09 16:44:36 +0100

    Adds sorting features to the parent element (for caching)

--- src/components/MapTable/MapTable.js
+++ src/components/MapTable/MapTable.js
@@ -44,7 +44,8 @@ export class MapTable extends Component<MapTableProps, {}> {
         geoData: null,
         loading: false,
         mapObject: null,
-        hash: ""
+        hash: "",
+        tableSort: {}
 
     } // state
 
@@ -88,11 +89,28 @@ export class MapTable extends Component<MapTableProps, {}> {
 
     }; // setCategory
 
-    tabClickHandler = (event, category) => {
+    // FixMe
+    tabClickHandler = (event, category: string): void => {
 
         this.setCategory(category)
 
     }; // tabClickHandler
+
+    tableSortHandler = (event: any, field: string, ascending: boolean): void => {
+
+        event.preventDefault();
+
+        this.setState(prevState => ({
+            tableSort: {
+                ...prevState.tableSort,
+                [utils.getParams(prevState.hash).category]: {
+                    field: field,
+                    ascending: ascending
+                }
+            }
+        }))
+
+    }; // tableSortHandler
 
     render(): React.ReactNode {
 
@@ -105,13 +123,14 @@ export class MapTable extends Component<MapTableProps, {}> {
                 geoData,
                 loading,
                 hash: locHash = "",
+                tableSort
             } = this.state,
             { children, isMobile = false } = this.props,
             hash = locHash !== ""
                 ? locHash
                 : utils.createHash({category: category, map: viewMapAs}),
             parsedHash = utils.getParams(hash),
-            contentData = Content.filter(item => item.textLabel === parsedHash.category)[0],
+            contentData = Content.filter(item => item.textLabel === parsedHash.category).pop(),
             resetHash = utils.createHash({ category: parsedHash.category, map: parsedHash.map });
 
 
@@ -139,6 +158,13 @@ export class MapTable extends Component<MapTableProps, {}> {
                     <div className={ `govuk-tabs__panel` } id={ parsedHash.category }>
                         <Table{ ...contentData }
                               hash={ hash }
+                              sortBy={
+                                  tableSort?.[parsedHash.category] ?? {
+                                      field: contentData.headings?.[0]?.id ?? "",
+                                      ascending: true
+                                  }
+                              }
+                              sortUpdate={ this.tableSortHandler }
                               populationData={ populationData }
                               data={ data?.[parsedHash.category] ?? null }
                               dataSetter={ (data) => this.setState(prevState => ({
