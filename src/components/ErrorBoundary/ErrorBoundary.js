0fad389d63d4f25446bea2dda52bc41be7a2ee79
Biggs Thorarensen <biggs@devillion.com>
2020-05-22 10:06:02 +0000

    error boundary component now has a UI with error details as well.

--- src/components/ErrorBoundary/ErrorBoundary.js
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -2,32 +2,55 @@
 
 import React from 'react';
 
-type Props = {
-  children: any,
-};
+import PageTitle from 'components/PageTitle';
+import type { Props } from './ErrorBoundary.types';
+import * as Styles from './ErrorBoundary.styles';
 
-type State = {
-  hasError: boolean
-};
 
 export default class ErrorBoundary extends React.Component<Props, State> {
-  props: Props;
-  state: State = {
-    hasError: false,
+    props: Props;
+    state: State = {
+        error: null,
+        errorInfo: null
   };
 
-  componentDidCatch() {
-    this.setState({
-      hasError: true,
-    });
+  componentDidCatch(error, errorInfo) {
+        // Catch errors in any components below and re-render with error message
+        this.setState({
+            error: error,
+            errorInfo: errorInfo
+        })
+        // You can also log error messages to an error reporting service here
   }
 
   render() {
-    const { hasError } = this.state;
+        if (this.state.errorInfo) {
 
-    if (hasError) {
-      return null;
+            // Error path
+            return <Styles.Container className="govuk-width-container" role="main">
+                <PageTitle title={"Something went wrong"} />
+
+                <p className={"govuk-body"}>
+                    There was an error. Please try again later.
+                </p>
+                <details className="govuk-details" data-module="govuk-details">
+                    <summary className="govuk-details__summary">
+                        <span className="govuk-details__summary-text">
+                            Technical details
+                        </span>
+                    </summary>
+                    <Styles.DetailsBody className="govuk-details__text">
+                        {this.state.error && this.state.error.toString()}
+                        <br/>
+                        {this.state.errorInfo.componentStack}
+                    </Styles.DetailsBody>
+                </details>
+
+            </Styles.Container>
+
+        }
+        // Normally, just render children
+        return this.props.children;
     }
-    return this.props.children;
-  }
+
 }
