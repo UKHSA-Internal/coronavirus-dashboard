6b8ba360f758f35da701db24d37627dec5c5ff48
Biggs Thorarensen <biggs@devillion.com>
2020-05-22 11:00:21 +0000

    error content improved

--- src/components/ErrorBoundary/ErrorBoundary.js
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -30,9 +30,19 @@ export default class ErrorBoundary extends React.Component<Props, State> {
             return <Styles.Container className="govuk-width-container" role="main">
                 <PageTitle title={"Something went wrong"} />
 
-                <p className={"govuk-body"}>
+                <p className="govuk-body">
                     There was an error. Please try again later.
                 </p>
+                <p className="govuk-body">
+                    If the problem persists, please contact us via <a href="mailto:coronavirus-tracker@phe.gov.uk" className="govuk-link">coronavirus-tracker@phe.gov.uk</a> and include:
+                </p>
+                <ul className="govuk-list govuk-list--bullet">
+                    <li>Details of what you were trying to do that caused the problem</li>
+                    <li>Your operating system (such as Windows, Mac OS, Android, iOS) and its version if possible</li>
+                    <li>Your platform (such as mobile, table, computer)</li>
+                    <li>Your browser (such as Chrome, Edge, Firefox, Internet Explorer)</li>
+                    <li>The technical details quoted below</li>
+                </ul>
                 <details className="govuk-details" data-module="govuk-details">
                     <summary className="govuk-details__summary">
                         <span className="govuk-details__summary-text">
