abfe225383c63670f66c877be2e5ab8566ffcb7a
Biggs Thorarensen <biggs@devillion.com>
2020-05-29 14:34:19 +0000

    navigation: dynamic active item

--- src/components/SideNavigation/SideNavigation.js
+++ src/components/SideNavigation/SideNavigation.js
@@ -10,43 +10,41 @@ import type { Props } from './SideNavigation.types';
 import * as Styles from './SideNavigation.styles';
 
 const SideNavigation: ComponentType<Props> = ({ location: { pathname }}: Props) => {
-  const layout = useResponsiveLayout(768);
+    const layout = useResponsiveLayout(768);
 
-  if (layout === 'desktop') {
-    return (
-      <nav class="moj-side-navigation govuk-!-padding-right-4 govuk-!-padding-top-2" aria-label="Side navigation">
-        <ul class="moj-side-navigation__list">
-          <li class="moj-side-navigation__item moj-side-navigation__item--active">
-            <a href="index" aria-current="location">Daily summary</a>
-          </li>
+    if (layout === 'desktop') {
+        return (
+            <nav className="moj-side-navigation govuk-!-padding-right-4 govuk-!-padding-top-2" aria-label="Side navigation">
+                <ul className="moj-side-navigation__list">
+                    <li className={`moj-side-navigation__item ${pathname === '/' ? "moj-side-navigation__item--active" : ""}`}>
+                        <a href="/" aria-current="location">Daily summary</a>
+                    </li>
 
-          <li class="moj-side-navigation__item">
-            <a href="tests">Tests</a>
-          </li>
+                    <li className={`moj-side-navigation__item ${pathname === '/tests' ? "moj-side-navigation__item--active" : ""}`}>
+                        <a href="tests">Tests</a>
+                    </li>
 
-          <li class="moj-side-navigation__item">
-            <a href="cases">Cases</a>
-          </li>
+                    <li className={`moj-side-navigation__item ${pathname === '/cases' ? "moj-side-navigation__item--active" : ""}`}>
+                        <a href="cases">Cases</a>
+                    </li>
 
-          <li class="moj-side-navigation__item">
-            <a href="healthcare">Healthcare</a>
-          </li>
+                    <li className={`moj-side-navigation__item ${pathname === '/healthcare' ? "moj-side-navigation__item--active" : ""}`}>
+                        <a href="healthcare">Healthcare</a>
+                    </li>
 
-          <li class="moj-side-navigation__item">
-            <a href="deaths">Deaths</a>
-          </li>
+                    <li className={`moj-side-navigation__item ${pathname === '/deaths' ? "moj-side-navigation__item--active" : ""}`}>
+                        <a href="deaths">Deaths</a>
+                    </li>
 
-          <hr class="govuk-section-break govuk-section-break--m govuk-!-margin-top-3 govuk-!-margin-bottom-3 govuk-section-break--visible" />
+                    <hr class="govuk-section-break govuk-section-break--m govuk-!-margin-top-3 govuk-!-margin-bottom-3 govuk-section-break--visible" />
 
-          <li class="moj-side-navigation__item">
-            <a href="about-data">About the data</a>
-          </li>
-
-        </ul>
-
-      </nav>
-    );
-  }
+                    <li className={`moj-side-navigation__item ${pathname === '/about' ? "moj-side-navigation__item--active" : ""}`}>
+                        <a href="about-data">About the data</a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
   return (
     <Styles.Container className="govuk-header__container--full-width">
       <Styles.NavList>
