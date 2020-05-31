ebbcea36f69f08452e4c5649a71ba5c21fb9b870
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-05-31 16:16:29 +0100

    Replace a-links with router-dom Link comps

--- src/components/SideNavigation/SideNavigation.js
+++ src/components/SideNavigation/SideNavigation.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import type { ComponentType } from 'react';
 import { withRouter } from 'react-router';
+import { Link } from "react-router-dom";
 
 import useResponsiveLayout from 'hooks/useResponsiveLayout';
 
@@ -17,29 +18,29 @@ const SideNavigation: ComponentType<Props> = ({ location: { pathname }}: Props) 
             <nav className="moj-side-navigation govuk-!-padding-right-4 govuk-!-padding-top-2" aria-label="Side navigation">
                 <ul className="moj-side-navigation__list">
                     <li className={`moj-side-navigation__item ${pathname === '/' ? "moj-side-navigation__item--active" : ""}`}>
-                        <a href="/" aria-current="location">Daily summary</a>
+                        <Link to={ "/" } aria-current="location">Daily summary</Link>
                     </li>
 
                     <li className={`moj-side-navigation__item ${pathname === '/tests' ? "moj-side-navigation__item--active" : ""}`}>
-                        <a href="tests">Tests</a>
+                        <Link to={ "tests" }>Tests</Link>
                     </li>
 
                     <li className={`moj-side-navigation__item ${pathname === '/cases' ? "moj-side-navigation__item--active" : ""}`}>
-                        <a href="cases">Cases</a>
+                        <Link to={ "cases" }>Cases</Link>
                     </li>
 
                     <li className={`moj-side-navigation__item ${pathname === '/healthcare' ? "moj-side-navigation__item--active" : ""}`}>
-                        <a href="healthcare">Healthcare</a>
+                        <Link to={ "healthcare" }>Healthcare</Link>
                     </li>
 
                     <li className={`moj-side-navigation__item ${pathname === '/deaths' ? "moj-side-navigation__item--active" : ""}`}>
-                        <a href="deaths">Deaths</a>
+                        <Link to={ "deaths" }>Deaths</Link>
                     </li>
 
                     <hr className="govuk-section-break govuk-section-break--m govuk-!-margin-top-3 govuk-!-margin-bottom-3 govuk-section-break--visible" />
 
                     <li className={`moj-side-navigation__item ${pathname === '/about' ? "moj-side-navigation__item--active" : ""}`}>
-                        <a href="about-data">About the data</a>
+                        <Link to={ "about-data" }>About the data</Link>
                     </li>
                 </ul>
             </nav>
