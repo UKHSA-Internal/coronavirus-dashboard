bf4805e8325e7ccc7b1ba7dc8d352c2040ce35ed
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-05-31 14:30:24 +0100

    Removes global defnitions

--- src/pages/Deaths/Deaths.js
+++ src/pages/Deaths/Deaths.js
@@ -3,9 +3,6 @@
 import React, { useState } from 'react';
 import type { ComponentType } from 'react';
 
-import PageTitle from 'components/PageTitle';
-import SideNavigation from 'components/SideNavigation';
-import DashboardHeader from 'components/DashboardHeader';
 import { HalfWidthCard } from 'components/Card';
 import type { Props } from './Deaths.types';
 import * as Styles from './Deaths.styles';
@@ -18,29 +15,11 @@ const Deaths: ComponentType<Props> = ({ }: Props) => {
     base.href = document.location.pathname;
 
 
-    return (
-        <div className="govuk-grid-row">
-            <div className="govuk-grid-column-full">
+    return <Styles.FlexContainer>
+        <HalfWidthCard />
+        <HalfWidthCard />
+    </Styles.FlexContainer>
 
-                <p className="govuk-body">Last updated on Thursday 28 May 2020 at 3:37pm</p>
-
-                <div class="govuk-grid-column-menu">
-                    <SideNavigation />
-                </div>
-
-                <div class="govuk-grid-column-dashboard">
-                    <DashboardHeader title={"Deaths"} />
-
-                    <Styles.FlexContainer>
-                        <HalfWidthCard />
-                        <HalfWidthCard />
-                    </Styles.FlexContainer>
-                </div>
-
-            </div>
-        </div>
-
-    );
 };
 
 export default Deaths
