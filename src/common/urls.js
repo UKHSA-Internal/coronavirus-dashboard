d7347f5317278baccd1d1f3a458238715cb2600a
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-05-31 14:32:03 +0100

    Adds new asset URLs

--- src/common/urls.js
+++ src/common/urls.js
@@ -15,6 +15,8 @@ const URLs = {
     baseGeo:        `https://${ REACT_APP_MAIN_CDN      }/assets/geo/`,
     mapStyle:       `https://${ REACT_APP_MAIN_CDN      }/assets/map/style_v2.json`,
     landingData:    `https://${ REACT_APP_DOWNLOADS_CDN }/downloads/data/landing.json`,
+    timestamp:      `https://${ REACT_APP_DOWNLOADS_CDN }/downloads/db/latest_timestamp`,
+    lookups:        `https://${ REACT_APP_MAIN_CDN }/assets/lookups/`,
     latestCases: {
         csv:  `${ REACT_APP_BASE_URL }/downloads/csv/coronavirus-cases_latest.csv`,
         json: `${ REACT_APP_BASE_URL }/downloads/json/coronavirus-cases_latest.json`
