3fe3a35b980cb7ab650e262e0cf64242c7739772
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-02 20:17:02 +0100

    restructuring + timestamp implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import React,  { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router';
 import { Header, Footer } from 'govuk-react-jsx';
 
@@ -29,35 +29,41 @@ import DashboardHeader from "./components/DashboardHeader";
 dotenv.config();
 
 
-(async () => {
+const useTimestamp = () => {
 
-    const { data } = await axios.get(URLs.timestamp, { responseType: 'text' })
+    const [ timestamp, setTimestamp ] = useState("");
 
-    sessionStorage.setItem("lastUpdate", data)
+    useEffect(() => {
+        (async () => {
+            const { data } = await axios.get(URLs.timestamp, { responseType: 'text' })
+            setTimestamp(data)
+        })();
+    }, []);
 
-})();
+    return timestamp
+
+};  // useTimestamp
+
 
 const LastUpdateTime = () => {
 
-    const timestamp = sessionStorage.getItem('lastUpdate');
+    const timestamp = useTimestamp();
 
     if (!timestamp) return null;
 
     return <p className={ "govuk-body-s govuk-!-margin-top-0 govuk-!-margin-bottom-5" }>
-        Last updated on { moment(timestamp).format("dddd D MMMM YYYY [at] h:mma") }
+        Last updated on&nbsp;{ moment(timestamp).format("dddd D MMMM YYYY [at] h:mma") }
     </p>
 
+}; // LastUpdateTime
 
-}
 
-
-const FooterContents = () => {
-
-    return <Fragment>
+const FooterContents = () => (
+    <Fragment>
         <p className={ "govuk-footer__meta-custom" }>
             For feedback email&nbsp;
             <a className="govuk-footer__link"
-               href={encodeURI("mailto:coronavirus-tracker@phe.gov.uk?Subject=Coronavirus dashboard feedback")}
+               href={ encodeURI("mailto:coronavirus-tracker@phe.gov.uk?Subject=Coronavirus dashboard feedback") }
                rel="noopener noreferrer"
                target="_blank"
             >coronavirus-tracker@phe.gov.uk</a>
@@ -77,75 +83,69 @@ const FooterContents = () => {
             >NHSX</a>
         </p>
     </Fragment>
-
-}; // FooterContents
-
-
-const F = () => <Footer
-
-    meta={ {
-        children: <FooterContents/>,
-        items: [
-            { children: ['Archive'], href: '/archive' },
-            { children: ['Accessibility'], href: '/accessibility' },
-            { children: ['Cookies'], href: '/cookies' }
-        ],
-        visuallyHiddenTitle: 'Items',
-    } }
-/>;
+);  // FooterContents
 
 
-const DashboardLayout = ({component: Component, ...rest}) => {
-  return (
-    <Route {...rest} render={matchProps => ( <Fragment>
-        <SideNavMobile />
-        <div className={ "govuk-width-container" }>
-            <main className={ "govuk-main-wrapper" } role={ "main" }>
-                <Switch>
-                    <Route path={ "/" } component={ LastUpdateTime }/>
-                </Switch>
-                <div className={ "dashboard-container" }>
-                    <div className={ "dashboard-menu" }>
-                        <Switch>
-                            <Route path={ "/" } component={ SideNavigation }/>
-                        </Switch>
-                    </div>
-                    <div className={ "dashboard-content" }>
-                        <DashboardHeader title={ "Daily Summary" } />
-                        <Switch>
-                            {/* These back-to-top links are the 'overlay' style that stays on screen as we scroll. */}
-                            <Route path="/" render={ () => <BackToTop mode={ "overlay" }/> } />
-                        </Switch>
+const F = () => (
+    <Footer
+        meta={ {
+            children: <FooterContents/>,
+            items: [
+                { children: ['Archive'], href: '/archive' },
+                { children: ['Accessibility'], href: '/accessibility' },
+                { children: ['Cookies'], href: '/cookies' }
+            ],
+            visuallyHiddenTitle: 'Items',
+        } }
+    />
+);  // Footer
 
-                        <Switch>
-                            <Component {...matchProps} />
-                            <Redirect to="/"/>
-                        </Switch>
-                    </div>
+
+const DashboardLayout = ({ component: Component, ...props }) => (
+    <Route { ...props } component={ matchProps =>
+        <Fragment>
+            <SideNavMobile/>
+            <Switch>
+                <Route path={ "/" } component={ LastUpdateTime }/>
+            </Switch>
+            <div className={ "dashboard-container" }>
+                <div className={ "dashboard-menu" }>
+                    <Switch>
+                        <Route path={ "/" } component={ SideNavigation }/>
+                    </Switch>
                 </div>
-            </main>
-        </div></Fragment>
-    )} />
-  )
-};
+                <div className={ "dashboard-content" }>
+                    <DashboardHeader title={ "Daily Summary" }/>
+                    <Switch>
+                        {/* These back-to-top links are the 'overlay' style that stays on screen as we scroll. */ }
+                        <Route path="/" render={ () => <BackToTop mode={ "overlay" }/> }/>
+                    </Switch>
+
+                    <Switch>
+                        <Component { ...matchProps } />
+                        <Redirect to={ "/" }/>
+                    </Switch>
+                </div>
+            </div>
+        </Fragment>
+    }/>
+);  // DashboardLayout
 
 
-const SimpleLayout = ({component: Component, ...rest}) => {
-  return (
-    <Route {...rest} render={matchProps => (
+const SimpleLayout = ({ component: Component, ...rest }) => (
+    <Route { ...rest } render={ matchProps => (
         <Fragment>
             <Switch>
-                {/* These back-to-top links are the 'overlay' style that stays on screen as we scroll. */}
-                <Route path="/" render={ () => <BackToTop mode={ "overlay" }/> } />
+                {/* These back-to-top links are the 'overlay' style that stays on screen as we scroll. */ }
+                <Route path="/" render={ () => <BackToTop mode={ "overlay" }/> }/>
             </Switch>
             <Switch>
-                <Component {...matchProps} />
+                <Component { ...matchProps } />
                 <Redirect to="/"/>
             </Switch>
         </Fragment>
-    )} />
-  )
-};
+    ) }/>
+);  // SimpleLayout
 
 
 const App = () => {
@@ -160,6 +160,8 @@ const App = () => {
             homepageUrlHref="https://gov.uk"
         />
 
+        <div className={ "govuk-width-container" }>
+            <main className={ "govuk-main-wrapper" } role={ "main" }>
                 <ErrorBoundary>
                     <DashboardLayout path="/" exact component={ DailySummary }/>
                     <DashboardLayout path="/tests" component={ Tests }/>
@@ -167,25 +169,26 @@ const App = () => {
                     <DashboardLayout path="/healthcare" component={ Healthcare }/>
                     <DashboardLayout path="/deaths" component={ Deaths }/>
                     <DashboardLayout path="/about-data" component={ About }/>
+
                     <SimpleLayout path="/archive" component={ Archive }/>
                     <SimpleLayout path="/accessibility" component={ Accessibility }/>
                     <SimpleLayout path="/cookies" component={ Cookies }/>
                 </ErrorBoundary>
+            </main>
+        </div>
 
-
-        {/* We only want back-to-top links on the main & about pages. */}
+        {/* We only want back-to-top links on the main & about pages. */ }
         <Switch>
             {/* These back-to-top links are the 'inline' style that sits
-                statically between the end of the content and the footer. */}
-            {/*<Route path="/about-data" exact render={ props => <BackToTop {...props} mode="inline"/> } />*/}
-            <Route path="/" render={ props => <BackToTop {...props} mode="inline"/>  } />
+                statically between the end of the content and the footer. */ }
+            <Route path="/" render={ props => <BackToTop { ...props } mode="inline"/> }/>
         </Switch>
 
         <Switch>
             <Route path="/" component={ F }/>
         </Switch>
     </Fragment>
-}
+};  // App
 
 
 export default App;
