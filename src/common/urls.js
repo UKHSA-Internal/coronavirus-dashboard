a26a1cb321578a2e54dde8fc5972ea669bbad4af
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-02 19:00:36 +0100

    adds API's endpoint to URLs

--- src/common/urls.js
+++ src/common/urls.js
@@ -17,6 +17,7 @@ const URLs = {
     landingData:    `https://${ REACT_APP_DOWNLOADS_CDN }/downloads/data/landing.json`,
     timestamp:      `https://${ REACT_APP_DOWNLOADS_CDN }/downloads/db/latest_timestamp`,
     lookups:        `https://${ REACT_APP_MAIN_CDN }/assets/lookups/`,
+    api:            `https://uks-covid19-pubdash-dev.azure-api.net/fn-coronavirus-dashboard-pipeline-etl-dev/v1/data`,
     latestCases: {
         csv:  `${ REACT_APP_BASE_URL }/downloads/csv/coronavirus-cases_latest.csv`,
         json: `${ REACT_APP_BASE_URL }/downloads/json/coronavirus-cases_latest.json`
