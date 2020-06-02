1f3943ce60aae01a0126035da9dda75434d22080
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-03 00:07:03 +0100

    implements generic styles for the tabs

--- src/components/TabLink/TabLink.styles.js
+++ src/components/TabLink/TabLink.styles.js
@@ -6,14 +6,26 @@ import type { ComponentType } from 'react';
 
 export const MainContainer: ComponentType<*> = (() => {
 
-    return styled.div``;
+    return styled.div`
+    display: flex;
+    flex-direction: column;
+    `;
 
 })();  // Container
 
 
 export const Body: ComponentType<*> = (() => {
 
-    return styled.div``;
+    return styled.div`
+    width: 100%;
+    
+    &.inactive {
+        // Do not use "display: none" - It confuses
+        // the graph's responsiveness.
+        height: 0;
+        visibility: hidden !important;
+    }
+    `;
 
 })();  // Body
 
@@ -28,9 +40,25 @@ export const TabsContainer: ComponentType<*> = (() => {
 export const Tab: ComponentType<*> = (() => {
 
     return styled.button`
+    padding: .5rem 1rem;
     outline: none;
     cursor: pointer;
     color: blue;
+    margin-bottom: 5px;
+    margin-right: 5px;
+    border-color: blue;
+    
+    &.active {
+        margin-bottom: 0 !important;
+        border-bottom: 5px solid !important;
+    }
+    
+    &:hover,
+    &:active, 
+    &:focus {
+        margin-bottom: 4px;
+        border-bottom: 1px solid;
+    }
     `;
 
 })();  // Tab
