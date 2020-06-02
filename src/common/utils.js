25dcbbbdc69e58dc2a4920165df658eee9b6f59d
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-03 00:06:21 +0100

    implements centred rolling average

--- src/common/utils.js
+++ src/common/utils.js
@@ -64,50 +64,33 @@ export const fillDateGaps = (data: Array<any>, defaultValue: number = 0): Array<
 /**
  * Moving average
  * --------------
- * Courtesy of `kaelzhang`_, under MIT License.
- *
- * .. _kaelzhang: https://github.com/kaelzhang/moving-averages
- *
- * With alterations and optimisation by Pouria Hadjibagheri.
+ * Centered moving average.
  *
  * @param data { Array<number> } Array of numbers
  * @param size { number } Size of the moving window
  * @returns { Array<number> } Array of moving averages
  */
 export const movingAverage = ( data: number[], size: number ): number[] => {
+
     const
-        length = data.length,
-        prepare = size - 1,
-        ret = Array(length).fill(NaN);
+        N = data.length,
+        floor = Math.floor(size / 2),
+        ceil = Math.ceil(size / 2);
 
     let
-        sum = 0,
-        i = 0,
-        counter = 0,
-        datum;
+        movingMean = Array(N).fill(NaN),
+        sum ;
 
-    for ( ; i < length && counter < prepare; i++ ) {
-        datum = data[i]
+    for ( let index = floor; index < N - size; index++ ) {
+        sum = 0;
 
-        if ( isNumeric(datum) ) {
-            sum += datum;
-            counter++
-        }
+        for ( let sumIndex = index - floor; sumIndex < index + ceil; sumIndex ++ )
+            sum = sum + data[sumIndex];
+
+        movingMean[index] = sum / size;
     }
 
-    for ( ; i < length; i++ ) {
-        datum = data[i]
-
-        if ( isNumeric(datum) )
-            sum += datum;
-
-        if ( isNumeric(data[i - size]) )
-            sum -= data[i - size];
-
-        ret[i] = sum / size
-    }
-
-    return ret.slice(size - 1)
+    return movingMean
 
 } // movingAverage
 
