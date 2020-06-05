a0207e857081f81d05d0136dfd37ddcf22f3b6f2
Biggs Thorarensen <biggs@devillion.com>
2020-06-05 16:32:18 +0000

    TabLink classes exported to SC

--- src/components/TabLink/TabLink.styles.js
+++ src/components/TabLink/TabLink.styles.js
@@ -1,52 +1,53 @@
 // @flow
 
+import React from "react";
 import styled from 'styled-components';
 import type { ComponentType } from 'react';
 
 
-export const MainContainer: ComponentType<*> = (() => {
-
-    return styled.div`
-    `;
-
-})();  // MainContainer
+export const MainContainer = ({ className, ...props }) => (
+  <div className={ `util-flex util-flex-col ${className}` } { ...props }/>
+);
 
 
-export const Body: ComponentType<*> = (() => {
-
-    return styled.div`
-    width: 100%;
-
-    &.inactive {
-        // Do not use "display: none" - It confuses
-        // the graph's responsiveness.
-        height: 0;
-        visibility: hidden !important;
-    }
-    `;
-
-})();  // Body
-
-
-export const TabsContainer: ComponentType<*> = (() => {
-
-    return styled.div``;
-
-})();  // TabsContainer
+export const TabsContainer = ({ className, ...props }) => (
+  <div className={ `util-flex util-flex-wrap util-flex-align-items-start govuk-!-margin-bottom-6 ${className}` } { ...props }/>
+);
 
 
 export const Tab: ComponentType<*> = (() => {
-
-    return styled.button`
-        color: #1E70B8;
-        padding-bottom: 2px;
-        cursor: pointer;
-
-        &.active {
+    const
+        classes = 'govuk-link govuk-!-font-size-19 govuk-!-margin-right-7',
+        Node = styled.button`
             color: #1E70B8;
-            border-bottom: 4px solid #1E70B8;
+            padding-bottom: 2px;
+            cursor: pointer;
+
+            &.active {
+                color: #1E70B8;
+                border-bottom: 4px solid #1E70B8;
+            }
+        `;
+
+  return ({ className, ...props }) =>
+    <Node className={ `${classes} ${className}` } { ...props }/>
+})();  // Tab
+
+
+export const Body: ComponentType<*> = (() => {
+    return styled.div`
+        width: 100%;
+
+        &.inactive {
+            // Do not use "display: none" - It confuses
+            // the graph's responsiveness.
+            height: 0;
+            visibility: hidden !important;
         }
     `;
+})();  // Body
 
-})();  // Tab
+
+
+
 
