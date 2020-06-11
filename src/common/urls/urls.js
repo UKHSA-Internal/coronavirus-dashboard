52e00bd7239a43eadd501f9455b932205958ee2f
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-12 00:28:00 +0100

    adds modal URLs

--- src/common/urls/urls.js
+++ src/common/urls/urls.js
@@ -10,15 +10,16 @@ const
 
 const URLs = {
 
-    populationData: `https://${ REACT_APP_MAIN_CDN      }/assets/population/population.json`,
-    baseData:       `https://${ REACT_APP_DOWNLOADS_CDN }/downloads/data/`,
-    baseGeo:        `https://${ REACT_APP_MAIN_CDN      }/assets/geo/`,
-    mapStyle:       `https://${ REACT_APP_MAIN_CDN      }/assets/map/style_v3.json`,
-    landingData:    `https://${ REACT_APP_DOWNLOADS_CDN }/downloads/data/landing.json`,
-    timestamp:      `https://${ REACT_APP_DOWNLOADS_CDN }/downloads/test/v2/info/latest_timestamp`,
-    lookups:        `https://${ REACT_APP_MAIN_CDN }/assets/lookups/`,
-    pageLayoutsBase:   `https://${ REACT_APP_MAIN_CDN }/assets/structural_layouts/`,
-    api:            `https://uks-covid19-pubdash-dev.azure-api.net/fn-coronavirus-dashboard-pipeline-etl-dev/v1/data`,
+    populationData:  `https://${ REACT_APP_MAIN_CDN      }/assets/population/population.json`,
+    baseData:        `https://${ REACT_APP_DOWNLOADS_CDN }/downloads/data/`,
+    baseGeo:         `https://${ REACT_APP_MAIN_CDN      }/assets/geo/`,
+    mapStyle:        `https://${ REACT_APP_MAIN_CDN      }/assets/map/style_v3.json`,
+    landingData:     `https://${ REACT_APP_DOWNLOADS_CDN }/downloads/data/landing.json`,
+    timestamp:       `https://${ REACT_APP_DOWNLOADS_CDN }/downloads/test/v2/info/latest_timestamp`,
+    lookups:         `https://${ REACT_APP_MAIN_CDN }/assets/lookups/`,
+    pageLayoutsBase: `https://${ REACT_APP_MAIN_CDN }/assets/structural_layouts/`,
+    api:             `https://uks-covid19-pubdash-dev.azure-api.net/fn-coronavirus-dashboard-pipeline-etl-dev/v1/data`,
+    modals:          `https://${ REACT_APP_MAIN_CDN }/assets/modals/`,
     latestCases: {
         csv:  `${ REACT_APP_BASE_URL }/downloads/csv/coronavirus-cases_latest.csv`,
         json: `${ REACT_APP_BASE_URL }/downloads/json/coronavirus-cases_latest.json`
