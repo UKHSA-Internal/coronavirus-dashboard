488ba66115471ef7f3a26aba9c64ea2a36074226
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-12 00:28:51 +0100

    adds ModalTooltip

---
+++ src/components/Modal/modal.js
@@ -0,0 +1,48 @@
+// @flow
+
+import React, { useState, Fragment } from "react";
+import { ModalContainer, ModalContent, ModalOpener, Markdown, ModalCloser } from "./modal.styles";
+import useModalData from "hooks/useModalData";
+import { strFormat } from "common/utils";
+import { MainLoading } from "components/Loading";
+
+
+const Modal = ({ markdownPath, replacements }) => {
+
+    const data = useModalData(markdownPath);
+
+    if ( !data ) return <MainLoading/>
+
+    return <Markdown dangerouslySetInnerHTML={{ __html: strFormat(data, replacements) }}/>
+
+}; // Modal
+
+
+const ModalTooltip = ({ markdownPath, children, replacements={}, ...props }) => {
+
+    const
+        [ modalStatus, setModalStatus ] = useState(false);
+
+    return <Fragment>
+        <ModalOpener { ...props }
+                     onClick={ () => setModalStatus(true) }
+                     children={ children }/>
+
+        {
+            modalStatus && <ModalContainer>
+                <ModalContent>
+                    <Modal markdownPath={ markdownPath } replacements={ replacements }/>
+                    <ModalCloser className={ "govuk-button" }
+                                 onClick={ () => setModalStatus(false) }>
+                        Continue
+                    </ModalCloser>
+                </ModalContent>
+            </ModalContainer>
+        }
+
+    </Fragment>
+
+}; // ModalTooltip
+
+
+export default ModalTooltip;
