ba1be39ccc2a2c5c7d83af2a708301bdd668fa99
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-12 11:59:24 +0100

    implements bar plot

--- src/pages/DailySummary/plots.js
+++ src/pages/DailySummary/plots.js
@@ -12,7 +12,7 @@ export const Plotter = ({ layout, ...props }) => {
             staticPlot: true
         } }
         useResizeHandler={ true }
-        style={{ display: 'flex' }}
+        style={{ display: 'block', height: 240 }}
         layout={ {
             // width: 400,
             height: 240,
@@ -57,3 +57,40 @@ export const Plotter = ({ layout, ...props }) => {
     />
 
 }; // Plotter
+
+
+export const BarPlotter = ({ data, ...props }) => {
+
+    return <Plotter
+        data={ data }
+        style={{ display: 'block', height: 140 }}
+        layout={{
+            margin: {
+                l: 57,
+                r: 0,
+                b: 0,
+                t: 0,
+                pad: 5
+            },
+            offset: .1,
+            yaxis: {
+                tickfont:{
+                    family: `"GDS Transport", Arial, sans-serif`,
+                    size : 13,
+                    color: "#6f777b",
+                },
+                layer: "below traces"
+            },
+            xaxis: {
+                visible: false,
+                layer: "below traces"
+            },
+            height: 140,
+            uniformtext: {
+                minsize: 8,
+                mode: 'hide'
+            }
+        }}
+    />
+
+};  // BarPlotter
