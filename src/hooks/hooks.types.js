0662bb6fc2c4b3b6a5be1fe2e6ad8deeaf836b31
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-14 19:57:45 +0100

    adds type hint for other inputs

--- src/hooks/hooks.types.js
+++ src/hooks/hooks.types.js
@@ -45,10 +45,34 @@ export type UKSummary = {
 };
 
 
+export type Testing = {||};
+export type Healthcare = {||};
+export type Deaths = {||};
+export type Cases = {||};
+
 export type UKSummaryInput = "UKSummary";
+export type TestingInput = "testing";
+export type HealthcareInput = "healthcare";
+export type DeathsInput = "deaths";
+export type CasesInput = "cases";
 
 export type usePageLayoutReturnType<T> =
-    T extends UKSummaryInput ? UKSummary : void;
+    T extends UKSummaryInput ? UKSummary :
+    T extends TestingInput ? Testing :
+    T extends HealthcareInput ? Healthcare :
+    T extends DeathsInput ? Deaths :
+    T extends CasesInput ? Cases : void
 
 
-export type usePageLayoutInputType = UKSummaryInput;
+export type LayoutInput =
+    UKSummaryField
+    | TestingInput
+    | HealthcareInput
+    | DeathsInput
+    | CasesInput;
+
+export type PageLayoutDefaultOutput = {
+    [any]: any
+} | Array<any>;
+
+declare export type usePageLayoutInputType<T> = (layout: LayoutInput, defaultOutput: PageLayoutDefaultOutput) => usePageLayoutReturnType<T>;
