0462885c5d428fa61103f6b96c810646d85ece5b
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-14 19:57:02 +0100

    substantial development + integration of layout files

--- src/pages/Cases/plots.js
+++ src/pages/Cases/plots.js
@@ -3,9 +3,87 @@ import Plot from "react-plotly.js";
 import URLs from "../../common/urls";
 
 
-export const Plotter = ({ layout={}, xaxis={}, yaxis={}, ...props }) => {
+// export const Plotter = ({ layout={}, xaxis={}, yaxis={}, ...props }) => {
+//
+//     return <Plot
+//         config={ {
+//             showLink: false,
+//             responsive: true,
+//             displaylogo: false,
+//             modeBarButtonsToRemove: [
+//                 "autoScale2d",
+//                 "zoomIn2d",
+//                 "zoomOut2d",
+//                 "toggleSpikelines",
+//                 "hoverClosestCartesian",
+//                 "zoom2d"
+//             ],
+//             toImageButtonOptions: {
+//                 format: 'png',
+//                 filename: 'export',
+//                 height: 989,
+//                 width: 1600,
+//                 scale: 4
+//             }
+//         } }
+//         useResizeHandler={ true }
+//         style={{ display: 'flex' }}
+//         layout={ {
+//             barmode: 'stack',
+//             height: 320,
+//             legend: {
+//                 orientation: 'h',
+//                 font: {
+//                     family: `"GDS Transport", Arial, sans-serif`,
+//                     size: 16,
+//                 },
+//                 xanchor: 'auto',
+//                 y: -.2
+//             },
+//             showlegend: true,
+//             margin: {
+//                 l: 60,
+//                 r: 25,
+//                 b: 40,
+//                 t: 20,
+//                 pad: 5
+//             },
+//             xaxis: {
+//                 showgrid: false,
+//                 zeroline: false,
+//                 showline: false,
+//                 // tickangle: 30,
+//                 type: "date",
+//                 tickfont:{
+//                     family: `"GDS Transport", Arial, sans-serif`,
+//                     size : 14,
+//                     color: "#6f777b"
+//                 },
+//                 ...xaxis
+//             },
+//             yaxis: {
+//                 tickformat: 's',
+//                 tickfont:{
+//                     family: `"GDS Transport", Arial, sans-serif`,
+//                     size : 14,
+//                     color: "#6f777b",
+//                 },
+//                 ...yaxis
+//             },
+//             plot_bgcolor: "rgba(231,231,231,0)",
+//             paper_bgcolor: "rgba(255,255,255,0)",
+//             ...layout
+//         } }
+//         {...props}
+//     />
+//
+// }; // Plotter
+
+
+export const Plotter = ({ data, layout={}, xaxis={}, yaxis={}, ...props }) => {
 
     return <Plot
+        data={ data }
         config={ {
             showLink: false,
             responsive: true,
@@ -24,13 +102,15 @@ export const Plotter = ({ layout={}, xaxis={}, yaxis={}, ...props }) => {
                 height: 989,
                 width: 1600,
                 scale: 4
-            }
+            },
+            // onLegendItem
         } }
         useResizeHandler={ true }
-        style={{ display: 'flex' }}
+        style={{ display: 'block', height: 320 }}
         layout={ {
-            barmode: 'stack',
-            height: 320,
+            // barmode: "overlay",
+            // barmode: "stack",
+            // height: 320,
             legend: {
                 orientation: 'h',
                 font: {
@@ -78,6 +158,9 @@ export const Plotter = ({ layout={}, xaxis={}, yaxis={}, ...props }) => {
     />
 
 }; // Plotter
+
+
+
 
 
 export const Choropleth = ({ data, layout, config, ...props }) => {
--- src/pages/Testing/Testing.types.js
+++ src/pages/Testing/Testing.types.js
@@ -1,3 +1,26 @@
 // @flow
 
 export type Props = {||};
+
+
+export type TabContentProps = {
+    params: any,
+    tabtype: string,
+    barmode:
+        null
+        | "stack"
+        | "normal"
+        | "group"
+        | "overlay",
+    fields: Array<{
+        label: string,
+        value: string,
+        tooltip: string,
+        type: string,
+        colour: number,
+        rollingAverage: boolean
+    }>
+}
+
+
+declare export type TabContentType<P> = React$ComponentType<P>;
