0b627a1c6472a6a6162a40181b97b37edf2e42f7
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-15 10:10:06 +0100

    incorporates generic data table component

--- src/pages/Deaths/Deaths.js
+++ src/pages/Deaths/Deaths.js
@@ -23,8 +23,7 @@ import { Plotter } from "components/Plotter";
 import usePageLayout from "hooks/usePageLayout";
 import URLs from "common/urls";
 import { MainLoading } from "components/Loading";
-import { Table } from "components/GovUk";
-import moment from "moment";
+import { DataTable } from "components/GovUk";
 import { Radios } from 'govuk-react-jsx';
 
 
@@ -33,7 +32,6 @@ const
         { key: 'areaName', sign: '=', value: 'United Kingdom' },
         { key: 'areaType', sign: '=', value: 'overview' }
     ];
-
 
 
 const Radio = ({ options, value, setValue }) => {
@@ -55,27 +53,6 @@ const Radio = ({ options, value, setValue }) => {
     />
 
 }
-
-
-const DataTable = ({ fields, data }) => {
-
-    const fieldNames = fields.map(item => item.value)
-
-    return <Table
-        head={[
-            fields.map(item => ({ value: item.label, type: item.type }))
-        ]}
-        body={
-            data.map(item => fieldNames.map(name =>
-                name === "date"
-                    ? moment(item[name]).format("DD-MM-YYYY")
-                    : item[name]
-
-            ))
-        }
-    />
-
-};  // DataTable
 
 
 const TabContent: TabContentType<TabContentProps> = ({ fields, params, tabType, barType=null }: TabContentProps): React$Component => {
