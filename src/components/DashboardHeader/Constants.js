8764bf51c9c58067d122e3531bea90b9d08131ae
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-19 01:12:34 +0100

    Adds constants module

---
+++ src/components/DashboardHeader/Constants.js
@@ -0,0 +1,34 @@
+// Global constants
+export const PathNameMapper = {
+    "/": "UK Summary",
+    "/cases": "Cases",
+    "/testing": "Testing",
+    "/healthcare": "Healthcare",
+    "/deaths": "Deaths",
+    "/about-data": "About data",
+    "/cookies": "Cookies",
+    "/accessibility": "Accessibility",
+    "/archive": "Archive"
+};
+
+export const PathNames = {
+    // Only use lower case in paths.
+    summary: "/",
+    cases: "/cases",
+    testing: "/testing",
+    healthcare: "/healthcare",
+    deaths: "/deaths",
+    aboutData: "/about-data",
+    cookies: "cookies",
+    accessibility: "/accessibility",
+    archive: "/archive"
+}
+
+
+export const NoPickerPaths = [
+    "/",
+    "/about-data",
+    "/cookies",
+    "/accessibility",
+    "/archive"
+];
