e167ebf856c2fb6471bd6199ef0af665eb4c6bd2
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-19 01:17:16 +0100

    implement lookup table (destinations) hook

---
+++ src/hooks/useDestinationLookup.js
@@ -0,0 +1,64 @@
+// @flow
+
+import React, { useState, useEffect, useRef } from "react";
+
+import axios from "axios";
+import URLs from "common/urls";
+import { createQuery } from "common/utils";
+
+import deepEqual from "deep-equal";
+
+
+const usePrevious = (value) => {
+
+    const ref = useRef([]);
+
+    useEffect(() => {
+        ref.current = value
+    })
+
+    return ref.current
+
+};  // usePrevious
+
+
+const useDestinationLookup = (conjunctiveFilters=[]) => {
+
+    const
+        [ lookupTable, setLookupTable ] = useState(null),
+        prevConjunctiveParams =  usePrevious(conjunctiveFilters);
+
+    useEffect(() => {
+        const
+            conjunctive = createQuery(conjunctiveFilters, ";", "") || "",
+            urlParams = createQuery([{
+                key: 'filters',
+                sign: '=',
+                value: conjunctive
+            }, {
+                key: 'structure',
+                sign: '=',
+                value: JSON.stringify({
+                    destinations: "destinations"
+                })
+            }]);
+
+        console.log(urlParams);
+
+        (async () => {
+
+            if ( conjunctive && !deepEqual(prevConjunctiveParams, conjunctiveFilters) ) {
+                const { data } = await axios.get(URLs.lookupApi + urlParams);
+                setLookupTable(data.data?.[0]?.destinations ?? {})
+            }
+        })()
+
+    });
+
+    console.log("lookup", lookupTable)
+    return lookupTable
+
+};  // GetLookup
+
+
+export default useDestinationLookup;
