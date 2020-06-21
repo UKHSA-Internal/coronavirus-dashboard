ba252fe3746d18941e0288dc33281ffdab71cadd
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-21 13:23:42 +0100

    Removes unused types

--- src/pages/Cases/Cases.types.js
+++ src/pages/Cases/Cases.types.js
@@ -4,9 +4,3 @@ import type { TabType } from "hooks/usePageLayout.types";
 
 
 export type Props = {||};
-
-export type TabContentProps = TabType & {
-    params: any,
-}
-
-declare export type TabContentType<P> = React$ComponentType<P>;
