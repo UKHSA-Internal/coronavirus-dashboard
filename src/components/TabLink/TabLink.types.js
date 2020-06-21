454840f7e851d00ade422da0c024032e55519720
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-21 13:25:39 +0100

    Adds TabContent and TabLinkContainer props

--- src/components/TabLink/TabLink.types.js
+++ src/components/TabLink/TabLink.types.js
@@ -1,16 +1,24 @@
-import type { React$Node } from "react";
+import type { ComponentType } from "react";
+import type { TabType } from "hooks/usePageLayout.types";
 
 
 export type TabLinkProps = {
 
     label: string,
-    children: React$Node
+    children: ComponentType
 
 };  // TabLinkProps
 
 
 export type TabLinkContainerProps = {
 
-    children: Array<React$Node>
+    children: Array<ComponentType>
 
 };  // TabLinkContainerProps
+
+
+export type TabContentProps = TabType & {
+    params: any,
+}
+
+declare export type TabContentType<P> = ComponentType<P>;
