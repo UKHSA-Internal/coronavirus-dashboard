514e65dabf3966c250a1ffc91784eed0c632527e
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-21 23:55:20 +0100

    Implements downloads

--- src/components/Card/Card.styles.js
+++ src/components/Card/Card.styles.js
@@ -3,6 +3,8 @@
 import React from "react";
 import styled from 'styled-components';
 import type { ComponentType } from 'react';
+import DownloadIcon from "assets/download.svg"
+
 
 export const HalfCard: ComponentType<*> = (() => {
     return styled.article`
@@ -109,6 +111,112 @@ export const DataColour: ComponentType<*> = (() => {
         box-shadow: inset 0 0 0 1px #fff;
         float: left;
     `;
+})();
+
+
+export const DownloadContainer: ComponentType<*> = (() => {
+
+    return styled.div`
+        min-width: 100px;
+        float: right;
+    `;
+
+})();
+
+
+export const DownloadOptionsContainer :ComponentType<*> = (() => {
+
+    return styled.div`
+        display: flex;
+        flex: 1 0 100px;
+        flex-direction: column;
+        position: absolute;
+        z-index: 999999999;
+        float: right;
+        margin-top: 14px;
+        margin-right: -15px !important;
+        width: 100px;
+        
+        & > * {
+            display: block;
+            padding: 8px 8px;
+            background-color: #f1f1f1;
+            border-top: 1px solid #e5e5e5;
+            border-left: 1px solid #e5e5e5;
+            border-right: 1px solid #e5e5e5;
+            width: 100px;
+            text-decoration: none;
+            text-align: left;
+            
+            &:active,
+            &:focus,
+            &:hover {
+                background-color: #e5e5e5;
+                outline: none;
+                box-shadow: none;
+            }
+            
+            &.disabled {
+                background-color: #f1f1f1;
+                color: #c1c1c1;
+            }
+            
+            &:first-child {
+                border-top-left-radius: 2px;
+                border-top-right-radius: 2px;
+            }
+            
+            &:last-child {
+                border-bottom: 1px solid #e5e5e5;
+                border-bottom-left-radius: 2px;
+                border-bottom-right-radius: 2px;
+            }
+        }
+        
+    `
+
+})();
+
+
+export const Download: ComponentType<*> = (() => {
+    const
+        Container = styled.span`
+            width: 33px;
+            height: 33px;
+            display: inline-block;
+            position: relative;
+            cursor: pointer;
+            margin: -20px;
+            float: right;
+            border-left: 1px solid #e8e8e8;
+            border-bottom: 1px solid #e8e8e8;
+            background-color: #f1f1f1;
+            transition: all .3s;
+            
+            &:hover {
+                background-color: #e1e1e1;
+            }
+        `,
+        Node = styled.button`
+            width: 28px;
+            height: 30px;
+            outline: none;
+            cursor: pointer;
+            display: inline-block;
+            float: right;
+            margin-right: 3px;
+            background: url("${ DownloadIcon }");
+            background-repeat: no-repeat;
+            background-size: 25px 25px;
+            background-position: center center;
+        `;
+
+    return ({ children, ...props }) => <Container>
+        <Node role={ "button" } { ...props }>
+            { children }
+        </Node>
+    </Container>
+
 })();
 
 
