61559b5b666d45605d5e9ac2c537724b6625e05d
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-22 11:07:53 +0100

    Adds stand-alone dropdown button component

---
+++ src/components/DropdownButton/DropdownButton.js
@@ -0,0 +1,63 @@
+import React, { useState, useRef, useEffect } from "react";
+
+import ReactTooltip from "react-tooltip";
+
+import {
+    Laucher,
+    DropdownContainer,
+    OptionsContainer
+} from "./DropdownButton.styles";
+
+import type { ComponentType } from "react";
+import type { DropdownButtonProps } from "./DropdownButton.types";
+
+
+const DropdownButton: ComponentType<DropdownButtonProps> = ({
+            tooltip, launcherSrOnly, children, optionsProps={},
+            launcherProps={}, ...props }) => {
+
+    const
+        [ open, setOpen ] = useState(false),
+        dropdown = useRef(null);
+
+    function handleClickEvent ({ target }) {
+
+        if ( dropdown.current && !target.closest(`.${dropdown.current.className}`) && open )
+            setOpen(false);
+
+    }
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickEvent);
+
+        return () => {
+            document.removeEventListener("click", handleClickEvent);
+        }
+    });
+
+    return <DropdownContainer ref={ dropdown } { ...props }>
+        <Laucher data-tip={ tooltip }
+                 data-for={ "tooltip-text" }
+                 className={ 'dropdown-launcher' }
+                 onClick={ () => setOpen(open => !open) }
+                 { ...launcherProps }>
+            <span className={ "govuk-visually-hidden" }>{ launcherSrOnly }</span>
+        </Laucher>
+        {
+            open &&
+            <OptionsContainer { ...optionsProps }>{ children }</OptionsContainer>
+        }
+        {
+            tooltip &&
+            <ReactTooltip id={ "tooltip-text" }
+                          place={ "right" }
+                          backgroundColor={ "#0b0c0c" }
+                          className={ "tooltip" }
+                          effect={ "solid" }/>
+        }
+    </DropdownContainer>
+
+};  // DownloadData
+
+
+export default DropdownButton;
