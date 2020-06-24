c175f034f6c05eef6d2692cdb8423e4e0ed77b8b
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-24 14:48:08 +0100

    new loading component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { PulseLoader } from "react-spinners";
+import { isIE } from "common/utils";
 
 
-export const MainLoading = () => {
+const Loading = ({ large=false }) => {
+
+    if ( isIE() ) return null;
+
+    if ( !large )
+        return <span>
+            <PulseLoader size={ 8 } margin={ 2 } color={ '#adadad' }/>
+            <span className={ "govuk-visually-hidden" }>The website is loading. Please wait.</span>
+        </span>
 
     return <div className="govuk-width-container" role="main">
         <div className={ "govuk-body govuk-!-font-size-24 govuk-!-margin-top-10" }>
             <PulseLoader size={ 12 } margin={ 2 } color={ '#adadad' }/>
-            The website is loading. Please wait&hellip;
+            <span className={ "govuk-visually-hidden" }>The website is loading. Please wait.</span>
         </div>
     </div>
 
 }; // MainLoading
+
+export default Loading;
