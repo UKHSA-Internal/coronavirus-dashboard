de68e3c211e6b848db19800bc57ed13148700e1b
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-24 22:45:12 +0100

    dev version bump + IE compatibility

--- src/components/DashboardHeader/DashboardHeader.js
+++ src/components/DashboardHeader/DashboardHeader.js
@@ -10,22 +10,22 @@ import { getParams } from "common/utils";
 
 import deepEqual from "deep-equal";
 
+import LocationPicker from "./LocationPicker";
 import { getParamValueFor, getParamDateFor } from "./utils";
 
 import { PathNameMapper, NoPickerPaths } from "./Constants";
 
-// import LocationPicker from "./LocationPicker";
-// import LocationProposer from "./LocationProposer";
+import LocationProposer from "./LocationProposer";
 
 // Styles
 import {
     MainContainer,
     HeaderContainer,
     Title,
-    // CollapsibleLinkContainer,
-    // CollapsibleLink,
+    CollapsibleLinkContainer,
+    CollapsibleLink,
     SectionBreak,
-    // CurrentLocation
+    CurrentLocation
 } from './DashboardHeader.styles'
 
 import type { ComponentType } from 'react';
@@ -49,51 +49,50 @@ const usePrevious = (value) => {
 const DashboardHeader: ComponentType<Props> = ({ title }: Props) => {
 
     const
-        history = useHistory();
-        // [locationPickerState, setLocationPickerState] = useState(false),
+        history = useHistory(),
+        [locationPickerState, setLocationPickerState] = useState(false),
         // [datePickerState, setDatePickerState] = useState(false),
-        // params = getParams(history.location.search),
-        // areaName = getParamValueFor(params, "areaName", "United Kingdom"),
-        // startDate = getParamDateFor(params, 'specimenDate', moment("2020-01-03"), ">"),
-        // endDate = getParamDateFor(params, "specimenDate", moment(), "<"),
-        // isExcluded = NoPickerPaths.indexOf(history.location.pathname) > -1,
-        // prevPathname = usePrevious(history.location.pathname),
-        // initialParam = getParams(history.location.query),
-        // [ prevPathnameState, setPrevPathnameState ] = useState(prevPathname),
-        // [ location, setLocation ] = useState({
-        //     areaType: getParamValueFor(initialParam, "areaType", "overview"),
-        //     areaName: getParamValueFor(initialParam, "areaName", "United Kingdom"),
-        // }),
-        // prevLocation = usePrevious(location);
-    //
-    // useEffect(() => {
-    //
-    //     if ( !deepEqual(location, prevLocation) )
-    //         setPrevPathnameState(prevPathname);
-    //
-    // }, [ location, history.location.pathname ])
+        params = getParams(history.location.search),
+        areaName = getParamValueFor(params, "areaName", "United Kingdom"),
+        startDate = getParamDateFor(params, 'specimenDate', moment("2020-01-03"), ">"),
+        endDate = getParamDateFor(params, "specimenDate", moment(), "<"),
+        isExcluded = NoPickerPaths.indexOf(history.location.pathname) > -1,
+        prevPathname = usePrevious(history.location.pathname),
+        initialParam = getParams(history.location.query),
+        [ prevPathnameState, setPrevPathnameState ] = useState(prevPathname),
+        [ location, setLocation ] = useState({
+            areaType: getParamValueFor(initialParam, "areaType", "overview"),
+            areaName: getParamValueFor(initialParam, "areaName", "United Kingdom"),
+        }),
+        prevLocation = usePrevious(location);
+
+    useEffect(() => {
+
+        if ( !deepEqual(location, prevLocation) )
+            setPrevPathnameState(prevPathname);
+
+    }, [ location, history.location.pathname ])
 
     return <MainContainer>
         <HeaderContainer>
             <Title>
                 { PathNameMapper[history.location.pathname] }
             </Title>
-            {/*{ !isExcluded &&*/}
-            {/*    <CollapsibleLinkContainer>*/}
-            {/*        <CollapsibleLink className={ locationPickerState ? "opened" : "closed" }*/}
-            {/*                         onClick={ () => setLocationPickerState(locationPickerState => !locationPickerState) }>*/}
-            {/*            Change&nbsp;location:&nbsp;<CurrentLocation>{ areaName }</CurrentLocation>*/}
-            {/*        </CollapsibleLink>*/}
-            {/*    </CollapsibleLinkContainer>*/}
-            {/*}*/}
-
-            {/*<CollapsibleLink htmlType={ "button" }*/}
-            {/*    onClick={ () => setDatePickerState(!datePickerState) }>*/}
-            {/*    { datePickerState ? <TriangleDown/> : <TriangleRight/> }*/}
-            {/*    <CollapsibleLinkText>*/}
-            {/*        <strong>Date:</strong>&nbsp;{ startDate.format("D MMM YYYY") }&nbsp;-&nbsp;{ endDate.format("D MMM YYYY") }*/}
-            {/*    </CollapsibleLinkText>*/}
-            {/*</CollapsibleLink>*/}
+            { !isExcluded &&
+                <CollapsibleLinkContainer>
+                    <CollapsibleLink className={ locationPickerState ? "opened" : "closed" }
+                                     onClick={ () => setLocationPickerState(locationPickerState => !locationPickerState) }>
+                        Change&nbsp;location:&nbsp;<CurrentLocation>{ areaName }</CurrentLocation>
+                    </CollapsibleLink>
+                </CollapsibleLinkContainer>
+            }
+                    {/*<CollapsibleLink htmlType={ "button" }*/}
+                    {/*    onClick={ () => setDatePickerState(!datePickerState) }>*/}
+                    {/*    { datePickerState ? <TriangleDown/> : <TriangleRight/> }*/}
+                    {/*    <CollapsibleLinkText>*/}
+                    {/*        <strong>Date:</strong>&nbsp;{ startDate.format("D MMM YYYY") }&nbsp;-&nbsp;{ endDate.format("D MMM YYYY") }*/}
+                    {/*    </CollapsibleLinkText>*/}
+                    {/*</CollapsibleLink>*/}
         </HeaderContainer>
         <SectionBreak/>
         {/*{*/}
@@ -103,21 +102,18 @@ const DashboardHeader: ComponentType<Props> = ({ title }: Props) => {
         {/*                           endDate={ endDate }/>*/}
         {/*        : null*/}
         {/*}*/}
+        {
+            !isExcluded &&
+            <>
+                <LocationPicker show={ locationPickerState } currentLocation={ location }
+                                setCurrentLocation={ setLocation }/>
 
-
-
-        {/*{*/}
-        {/*    !isExcluded &&*/}
-        {/*    <>*/}
-        {/*        <LocationPicker show={ locationPickerState } currentLocation={ location }*/}
-        {/*                        setCurrentLocation={ setLocation }/>*/}
-
-        {/*        <LocationProposer lastParams={ [*/}
-        {/*            { key: "areaType", sign: "=", value: location.areaType },*/}
-        {/*            { key: "areaName", sign: "=", value: location.areaName }*/}
-        {/*        ] } referrer={ prevPathnameState }/>*/}
-        {/*    </>*/}
-        {/*}*/}
+                <LocationProposer lastParams={ [
+                    { key: "areaType", sign: "=", value: location.areaType },
+                    { key: "areaName", sign: "=", value: location.areaName }
+                ] } referrer={ prevPathnameState }/>
+            </>
+        }
     </MainContainer>
 
 };  // DashboardHeader
