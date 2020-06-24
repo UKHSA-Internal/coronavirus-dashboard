9f85cf0b2b1710a57bb7c97c4e2c4bce431bdffc
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-24 23:36:32 +0100

    restores IE compatibility

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,15 +1,15 @@
+import 'react-app-polyfill/ie11';
 import React from "react";
 import { PulseLoader } from "react-spinners";
-import { InlineLoading } from "./Loading.styles";
 
 
 const Loading = ({ large=false }) => {
 
     if ( !large )
-        return <InlineLoading>
+        return <span>
             <PulseLoader size={ 8 } margin={ 2 } color={ '#adadad' }/>
             <span className={ "govuk-visually-hidden" }>The website is loading. Please wait.</span>
-        </InlineLoading>
+        </span>
 
     return <div className="govuk-width-container" role="main">
         <div className={ "govuk-body govuk-!-font-size-24 govuk-!-margin-top-10" }>
