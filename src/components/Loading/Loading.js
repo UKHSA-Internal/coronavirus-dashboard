c9bd0d70cd3df6f78990d4e8b8165bf0d3542180
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-06-24 22:54:47 +0100

    small loading now inline

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,15 +1,16 @@
 import 'react-app-polyfill/ie11';
 import React from "react";
 import { PulseLoader } from "react-spinners";
+import { InlineLoading } from "./Loading.styles";
 
 
 const Loading = ({ large=false }) => {
 
     if ( !large )
-        return <span>
+        return <InlineLoading>
             <PulseLoader size={ 8 } margin={ 2 } color={ '#adadad' }/>
             <span className={ "govuk-visually-hidden" }>The website is loading. Please wait.</span>
-        </span>
+        </InlineLoading>
 
     return <div className="govuk-width-container" role="main">
         <div className={ "govuk-body govuk-!-font-size-24 govuk-!-margin-top-10" }>
---
+++ src/components/Loading/Loading.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import type { ComponentType } from "react";
+
+
+export const InlineLoading: ComponentType<*> = (() => {
+
+    return styled.span`
+        display: inline-block;
+    `
+
+})();
