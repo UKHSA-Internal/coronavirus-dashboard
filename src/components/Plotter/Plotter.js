1077db2e3428493bf1dd22c7802da155d75225ad
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-07-03 01:14:48 +0100

    removes duplicate layout setting

--- src/components/Plotter/Plotter.js
+++ src/components/Plotter/Plotter.js
@@ -122,7 +122,6 @@ export const Plotter = ({ data, layout={}, xaxis={}, yaxis={}, config={}, margin
                 // barmode: "overlay",
                 // barmode: "stack",
                 // height: 320,
-                hovermode: "x unified",
                 legend: {
                     orientation: 'h',
                     font: {
