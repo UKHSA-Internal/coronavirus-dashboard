094f50565686839d193044e3766dabb878616ffd
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-07-03 13:33:48 +0100

    creates the Widgets module

---
+++ src/components/Widgets/Widgets.styles.js
@@ -0,0 +1,11 @@
+import type { ComponentType } from 'react';
+import styled from "styled-components";
+
+
+export const NotAvailable: ComponentType<*> = (() => {
+
+    return styled.span`
+      color: #808080;
+    `;
+
+})();
