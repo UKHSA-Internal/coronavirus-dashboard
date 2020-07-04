3cec29d06e18248b6332dd5ba3a47e7c7aa4dfe0
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-07-04 18:17:43 +0100

    announcement on the release of sub-national update

--- src/App.js
+++ src/App.js
@@ -45,8 +45,8 @@ const LastUpdateTime = () => {
     return <>
         <Announcement firstDisplayDate={{ day: 15, month: 6, year: 2020 }} lastDisplayDate={{ day: 1, month: 1, year: 2021 }}>
             <p className={ "govuk-body govuk-!-margin-top-0" }>
-                We are working to release sub-national data today. The deployment of the
-                latest data will be delayed.
+                We are working to fix a number of technical issues and will add
+                sub-national data as soon as possible.
             </p>
         </Announcement>
         <p className={ "govuk-body-s govuk-!-margin-top-5 govuk-!-margin-bottom-5" }>
