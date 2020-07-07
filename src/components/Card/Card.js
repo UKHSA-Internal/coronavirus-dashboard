dac4945452ca8f8a4f879b5a14cc28f63aba6e9b
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-07-07 21:06:37 +0100

    housekeeping

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import type { ComponentType } from 'react';
 import { Link } from "react-router-dom";
-import { renderToString } from 'react-dom/server'
 
 import ModalTooltip from "components/Modal";
 
