17fafb930275b59e2288f04ac95cb52c327d6716
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-07-08 00:40:46 +0100

    implements site footer as a new module

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,53 @@
+// @flow
+
+import React from "react";
+
+import { Footer as GovUKFooter } from "govuk-react-jsx";
+
+import type { ComponentType } from "react";
+
+
+const FooterContents: ComponentType<*> = () => (
+    <>
+        <p className={ "govuk-footer__meta-custom" }>
+            For feedback email&nbsp;
+            <a className="govuk-footer__link"
+               href={ encodeURI("mailto:coronavirus-tracker@phe.gov.uk?Subject=BETA dashboard feedback") }
+               rel="noopener noreferrer"
+               target="_blank"
+            >coronavirus-tracker@phe.gov.uk</a>
+        </p>
+        <p className={ "govuk-footer__meta-custom" }>
+            Developed by&nbsp;
+            <a className="govuk-footer__link"
+               href="https://www.gov.uk/government/organisations/public-health-england"
+               target="_blank"
+               rel="noopener noreferrer"
+            >Public Health England</a>
+            &nbsp;and&nbsp;
+            <a className="govuk-footer__link"
+               href="https://www.nhsx.nhs.uk/"
+               target="_blank"
+               rel="noopener noreferrer"
+            >NHSX</a>
+        </p>
+    </>
+);  // FooterContents
+
+
+const Footer = () => (
+    <GovUKFooter
+        meta={ {
+            children: <FooterContents/>,
+            items: [
+                // { children: ['Archive'], href: '/archive' },
+                { children: ['Accessibility'], href: '/accessibility' },
+                { children: ['Cookies'], href: '/cookies' }
+            ],
+            visuallyHiddenTitle: 'Items',
+        } }
+    />
+);  // Footer
+
+
+export default Footer;
