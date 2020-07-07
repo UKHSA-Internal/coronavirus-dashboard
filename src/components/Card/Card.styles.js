ac937b83c58915c70be623a0ac11330cce407ccb
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-07-07 23:13:19 +0100

    general housekeeping + moving components into independent modules

--- src/components/Card/Card.styles.js
+++ src/components/Card/Card.styles.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import styled from 'styled-components';
+
 import type { ComponentType } from 'react';
 
 
@@ -26,17 +27,24 @@ export const HalfCard: ComponentType<*> = (() => {
 })();
 
 
-export const HalfCardHeader = ({ className="", ...props }) => (
-    <header className={ `util-flex util-flex-justify-between util-flex-align-items-center govuk-!-padding-bottom-2 ${className}` } { ...props }/>
-);
+export const HalfCardHeader = ({ className="", ...props }) => {
+
+    return <header className={ `util-flex util-flex-justify-between util-flex-align-items-center govuk-!-padding-bottom-2 ${ className }` }
+                   { ...props }/>
+
+};
 
 
-export const HalfCardHeading = ({ className="", ...props }) => (
-    <h2 className={ `govuk-heading-m govuk-!-margin-bottom-0 ${className}` } { ...props }/>
-);
+export const HalfCardHeading = ({ className="", ...props }) => {
+
+    return <h2 className={ `govuk-heading-m govuk-!-margin-bottom-0 ${ className }` }
+               { ...props }/>
+
+};
 
 
 export const HalfCardSplitBody: ComponentType<*> = (() => {
+
     const
         classes = 'util-flex util-flex-wrap',
         Node = styled.div`
@@ -45,10 +53,12 @@ export const HalfCardSplitBody: ComponentType<*> = (() => {
 
     return ({ className="", ...props }) =>
         <Node className={ `${classes} ${className}` } { ...props }/>
+
 })();
 
 
 export const FullCard: ComponentType<*> = (() => {
+
     return styled.article`
         flex: 1 0 60%;
         padding: 20px;
@@ -61,114 +71,22 @@ export const FullCard: ComponentType<*> = (() => {
             margin-top: 0;
         }
     `;
+
 })();
 
 
-export const Caption = ({ className="", ...props }) => (
-    <span className={ `govuk-caption-m ${className}` } { ...props }/>
-);
+export const Caption = ({ className="", ...props }) => {
+
+    return <span className={ `govuk-caption-m ${className}` } { ...props }/>
+
+};
 
 
 export const BodySection: ComponentType<*> = (() => {
-        return styled.div`
-            flex: 1 1 43%;
-            margin: 0 15px;
-        `;
-})();
 
-
-
-export const HBodySection: ComponentType<*> = (() => {
-    return styled.section`
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: flex-start;
-        margin: 1rem 0 0 0;
-        
-        & > * {
-            margin-right: 2rem;
-            margin-bottom: 0;
-        }
-    `;
-})();
-
-
-export const DataContainer = ({ className="", ...props }) => (
-    <div className={ `${ className }` } { ...props }/>
-);
-
-
-export const Heading = ({ className="", ...props }) => (
-    <h3 className={ `govuk-heading-s govuk-!-margin-bottom-1 govuk-!-margin-left-4 ${className}` } { ...props }/>
-);
-
-
-export const DataColour: ComponentType<*> = (() => {
-    return styled.button`
-        cursor: pointer;
-        outline: none;
-        width: 12px;
-        height: 12px;
-        border: 1px solid #000;
-        margin: 6px 7px 6px 0;
-        background: ${props => props.colour};
-        box-shadow: inset 0 0 0 1px #fff;
-        float: left;
-    `;
-})();
-
-
-export const DataNumbersContainer = ({ className="", ...props }) => (
-    <div className={ `util-flex util-flex-wrap govuk-!-margin-bottom-21 govuk-!-margin-left-4 ${className}` } { ...props }/>
-);
-
-
-export const NumericData: ComponentType<*> = (() => {
-
-    const Node = styled.h3`
-        margin-top: 0;
-    `
-
-    return ({ className="", ...props }) =>
-        <Node className={ `govuk-!-margin-right-4 ${className}` } { ...props }/>
-})();
-
-
-export const DataLabel = ({ className="", ...props }) => (
-    <small className={ `govuk-caption-m govuk-!-font-size-16 ${className}` } { ...props }/>
-);
-
-
-export const Number: ComponentType<*> = (() => {
-
-    const Node = styled.span`
-        font-size: 1.4rem;
+    return styled.div`
+        flex: 1 1 43%;
+        margin: 0 15px;
     `;
 
-    return ({ className="", ...props }) =>
-        <Node className={ `govuk-heading-m govuk-!-font-weight-regular govuk-!-margin-bottom-2 govuk-!-padding-top-0 ${ className }` }
-            { ...props }/>
-
-})();
-
-
-export const AbstractContainer: ComponentType<*> = (() => {
-
-    const Node = styled.summary`
-        color: #626a6e;
-        
-        & .modal-opener-text {
-            color: #000;
-        }
-    `;
-
-    return ({ ...props }) =>
-        <div className={ "govuk-grid-row" }>
-            <div className={ "govuk-grid-column-one-half" }>
-                <div className={ "govuk-body govuk-body-m" }>
-                    <Node { ...props }/>
-                </div>
-            </div>
-        </div>
 })();
