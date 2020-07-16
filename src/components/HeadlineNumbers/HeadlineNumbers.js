aa0df53181ef3e19419518b33896714df9272fa9
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-07-16 13:04:08 +0100

    adopts new implementation based on unified component body

--- src/components/HeadlineNumbers/HeadlineNumbers.js
+++ src/components/HeadlineNumbers/HeadlineNumbers.js
@@ -5,30 +5,16 @@ import React from "react";
 import type { Props } from "./HeadlineNumbers.types";
 import ValueBox from "components/ValueBox";
 
-import { BodySection, HBodySection } from "./HeadlineNumbers.styles";
-
 import { ComponentType } from "react";
-
-
-export const NumericReports: ComponentType<Props> = ({ children, horizontal=false }: Props) => {
-
-    if ( horizontal )
-        return <HBodySection>{ children }</HBodySection>;
-
-    return <BodySection>{ children }</BodySection>
-
-}; // ValueItemContainer
 
 
 const HeadlineNumbers: ComponentType<Props> = ({ params, headlineNumbers=[] }) => {
 
-    return <NumericReports horizontal={ true }>{
-        headlineNumbers?.map((item, index) =>
+    return headlineNumbers?.map((item, index) =>
             <ValueBox params={ params }
                       key={ `headline-number-${index}` }
                       { ...item }/>
         ) ?? null
-    }</NumericReports>
 
 }; // HeadlineNumbers
 
