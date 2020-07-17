5ac5665735ba643cd49f69f2515e40c4250d7430
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-07-17 14:53:09 +0100

    API docs - initial

---
+++ src/pages/ApiDocs/ApiDocs.js
@@ -0,0 +1,22 @@
+// @flow
+
+import React from "react";
+
+import type { ComponentType } from "react";
+
+
+const ApiDocs: ComponentType<*> = ({ ...props }) => {
+
+    return <div>
+        <h1>Developer's Guide</h1>
+        <h2>Documentations for the API &mdash; v. 3</h2>
+        <h3>Endpoint</h3>
+        <p>
+            Our public API is available at
+        </p>
+    </div>
+
+};  // ApiDocs
+
+
+export default ApiDocs;
