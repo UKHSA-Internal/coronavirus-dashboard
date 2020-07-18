bf4841685e210f1058ec3cd2662dd7910b4eef7a
Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
2020-07-18 10:48:21 +0100

    improves abstract styling

--- src/components/Abstract/Abstract.styles.js
+++ src/components/Abstract/Abstract.styles.js
@@ -1,30 +1,36 @@
 // @flow
 
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import type { ComponentType } from "react";
 
 
 export const AbstractContainer: ComponentType<*> = (() => {
 
-    const Node = styled.p`   
-        color: #626a6e;
-             
-        & .modal-opener-text {
-            color: #000;
-            line-height: 2rem;
-            margin-bottom: 1.5rem;
-        }
-    `;
-
-    return ({ fullWidth, ...props }) => <div className={ "govuk-grid-row" }>
-        <div className={ fullWidth ? "govuk-grid-column-one-half" : "govuk-grid-column-two-thirds" }>
-            <div className={ "govuk-body-s" }>
-                <Node { ...props }/>
-            </div>
-        </div>
-    </div>
+    return styled
+        .summary
+        .attrs(({ className="" }) => ({
+            className: `${ className } govuk-body-s`
+        }))`   
+            color: #626a6e;
+            line-height: 1.7rem;
+            
+            ${
+                props => {
+                    console.log(props);
+                    return props.fullWidth
+                        ? css`width: 50% !important;`
+                        : css`width: 85%  !important;`
+                }
+            }
+                 
+            & .modal-opener-text {
+                color: #000;
+                line-height: 2rem;
+                margin-bottom: 1.5rem;
+            }
+        `;
 
 })();
 
