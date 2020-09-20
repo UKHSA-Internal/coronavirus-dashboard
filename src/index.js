2d4aa7b6674fd440adcf9d75b680c1286394c349
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2020-09-20 18:48:52 +0100

    adds static generator

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import 'react-app-polyfill/ie11';
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { initAll } from 'govuk-frontend'
+import { hydrate, render } from "react-dom";
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -15,15 +16,24 @@ import './index.scss';
 
 initAll()
 
-ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
+// ReactDOM.render(
+//     <Router>
+//       <App />
+//     </Router>,
+//
+//   // Bind via a class selector, to leave the element's id available for in-page
+//   // navigation.
+//   document.querySelector('.app-root'),
+// );
+//
 
-  // Bind via a class selector, to leave the element's id available for in-page
-  // navigation.
-  document.querySelector('.app-root'),
-);
+
+const rootElement = document.querySelector('.app-root');
+if (rootElement.hasChildNodes()) {
+  hydrate(<Router><App /></Router>, rootElement);
+} else {
+  render(<Router><App /></Router>, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
