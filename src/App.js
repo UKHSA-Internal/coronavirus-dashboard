aebfcccf58a38d297e9bd80db277992422293a78
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2020-11-22 11:05:33 +0000

    correct import statement for styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@
 import React, { useState, useEffect, lazy, Suspense } from 'react';
 import { Switch, Route, withRouter, Redirect, useParams } from 'react-router';
 import Header from "components/Header";
-// import DailySummary from 'pages/DailySummary';
 import BackToTop from 'components/BackToTop';
 import ErrorBoundary from "components/ErrorBoundary";
 import useTimestamp from "hooks/useTimestamp";
@@ -11,7 +10,7 @@ import moment from "moment";
 import useResponsiveLayout from "./hooks/useResponsiveLayout";
 import Loading from "components/Loading";
 
-import * as styles from './index.scss';
+import './index.scss';
 
 const
     DashboardHeader = lazy(() => import('components/DashboardHeader')),
