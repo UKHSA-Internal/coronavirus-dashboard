1cdc7cda444717c02aa998afad67721e0d2cead0
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2020-11-30 14:33:15 +0000

    moves form and formset to independent component

--- src/pages/Download/Download.styles.js
+++ src/pages/Download/Download.styles.js
@@ -21,7 +21,8 @@ export const DownloadLink: ComponentType<*> =
         .a
         .attrs(({ className="", href="", enabled=false }) => ({
             className: `govuk-link ${className}`,
-            href: `${href}`
+            href: `${href}`,
+            enabled
         }))`
             cursor: ${calcCursor};
             opacity: ${calcOpacity};
@@ -113,47 +114,6 @@ export const SideContent: ComponentType<*> =
             @media only screen and (min-width: 1100px) {
                 grid-column: 4/-1;
             }
-        `;
-
-
-export const Form: ComponentType<*> =
-    styled
-        .form`
-            display: grid;
-            grid-auto-flow: row;
-            grid-template-columns: repeat(4, 1fr);
-            margin-right: auto;
-            grid-row-gap: 2rem;
-            grid-column-gap: 1.5rem;
-            
-            & .one-half {
-                grid-column: 1/3;
-            }
-            
-            & .full {
-                grid-column: 1/-1;
-            }
-            
-            & .two-third {
-                grid-column: 1/4;
-            }
-            
-            @media only screen and (max-width: 1100px) {
-                & .one-half,
-                & .two-third {
-                    grid-column: 1/-1;
-                }
-            }
-        `;
-
-
-export const Formset: ComponentType<*> =
-    styled
-        .fieldset`
-            display: div;
-            border: unset;
-            border-left: ${ ({ error }) => error ? "5px solid #d4351c" : "unset" };
-            padding: ${ ({ error }) => error ? ".5rem 0 1rem 1rem" : "unset" };
         `;
 
 
