0d767a87b797c8a467aceb4614ffa9960abd7d89
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2020-12-01 13:23:30 +0000

    simplifies styles for better performance

--- src/components/BrowserHistory/BrowserHistory.styles.js
+++ src/components/BrowserHistory/BrowserHistory.styles.js
@@ -1,23 +1,24 @@
 // @flow
 
-import React from 'react';
 import styled from 'styled-components';
 import type { ComponentType } from 'react';
 
-export const Article: ComponentType<*> = (() => {
-    const
-        classes = 'about',
-        Node = styled.article`
-            max-width: 40em;
+
+export const Article: ComponentType<*> =
+    styled
+        .article
+        .attrs(({ className, ...props }) => ({
+            className: `about ${className}`,
+            ...props
+        }))`
+            max-width: 50em;
         `;
 
-  return ({ className, ...props }) =>
-    <Node className={ `${classes} ${className}` } { ...props }/>
-})();
 
-
-export const Markdown = ({ className, ...props }) => (
-    <div className={ `markdown ${className}` } { ...props }/>
-);
-
-
+export const Markdown: ComponentType<*> =
+    styled
+        .div
+        .attrs(({ className, ...props }) => ({
+            className: `markdown ${className}`,
+            ...props
+        }))``;
