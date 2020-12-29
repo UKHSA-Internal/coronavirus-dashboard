9a094b58515890f4f32ed7f12eb57380952a498b
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2020-12-29 15:36:17 +0000

    adopts location hook

--- src/components/SideNavMobile/SideNavMobile.js
+++ src/components/SideNavMobile/SideNavMobile.js
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import type { ComponentType } from 'react';
-import { withRouter } from 'react-router';
+import { useLocation } from 'react-router';
 import { Link } from "react-router-dom";
 
 import type { Props } from './SideNavMobile.types';
@@ -17,64 +17,65 @@ import {
 } from './SideNavMobile.styles';
 
 
-const SideNavMobile: ComponentType<Props> = ({ location: { pathname }}: Props) => {
+const SideNavMobile: ComponentType<Props> = ({ ...props }: Props) => {
+
     const [menuState, setMenuState] = useState(false);
-
-        return (
-            <MobileNavWrapper>
-                <MobileNavTogglerWrapper>
-                    <MobileNavToggler onClick={ () => setMenuState(state => !state) }>
-                        Menu ▼
-                    </MobileNavToggler>
-                </MobileNavTogglerWrapper>
-                { menuState ?
-                    <SideNav aria-label="Side navigation">
-                        <SideNavList>
-                            <SideNavListItem className={`${pathname === '/' ? "moj-side-navigation__item--active" : ""}`}>
-                                <a href={ "/" } aria-current="location">Daily update</a>
-                            </SideNavListItem>
-
-                            <SideNavListItem className={`${pathname === '/details/testing' ? "moj-side-navigation__item--active" : ""}`}>
-                                <Link to={ "/details/testing" }
-                                onClick={ () => setMenuState(state => !state) }>Testing</Link>
-                            </SideNavListItem>
-
-                            <SideNavListItem className={`${pathname === '/details/cases' ? "moj-side-navigation__item--active" : ""}`}>
-                                <Link to={ "/details/cases" }
-                                onClick={ () => setMenuState(state => !state) }>Cases</Link>
-                            </SideNavListItem>
-
-                            <SideNavListItem className={`${pathname === '/details/healthcare' ? "moj-side-navigation__item--active" : ""}`}>
-                                <Link to={ "/details/healthcare" }
-                                onClick={ () => setMenuState(state => !state) }>Healthcare</Link>
-                            </SideNavListItem>
-
-                            <SideNavListItem className={`${pathname === '/details/deaths' ? "moj-side-navigation__item--active" : ""}`}>
-                                <Link to={ "/details/deaths" }
-                                onClick={ () => setMenuState(state => !state) }>Deaths</Link>
-                            </SideNavListItem>
-
-                            <SideNavListItem className={`${pathname === '/details/about-data' ? "moj-side-navigation__item--active" : ""}`}>
-                                <Link to={ "/details/about-data" }
-                                onClick={ () => setMenuState(state => !state) }>About the data</Link>
-                            </SideNavListItem>
-                            <SideNavListItem className={`${pathname === '/details/download' ? "moj-side-navigation__item--active" : ""}`}>
-                                <Link to={ "/details/download" }
-                                onClick={ () => setMenuState(state => !state) }>Download data</Link>
-                            </SideNavListItem>
-                            <SideNavListItem className={`${pathname === '/details/developers-guide' ? "moj-side-navigation__item--active" : ""}`}>
-                                <Link to={ "/details/developers-guide" }
-                                onClick={ () => setMenuState(state => !state) }>Developer's guide</Link>
-                            </SideNavListItem>
-                        </SideNavList>
-                        <SectionBreak/>
-                    </SideNav>
-                    : ''
-                }
-            </MobileNavWrapper>
-        );
+    const { pathname } = useLocation();
 
 
-};
+    return <MobileNavWrapper { ...props }>
+        <MobileNavTogglerWrapper>
+            <MobileNavToggler onClick={ () => setMenuState(state => !state) }>
+                Menu ▼
+            </MobileNavToggler>
+        </MobileNavTogglerWrapper>{ menuState ?
+            <SideNav aria-label="Side navigation">
+                <SideNavList>
+                    <SideNavListItem className={`${pathname === '/' ? "moj-side-navigation__item--active" : ""}`}>
+                        <a href={ "/" } aria-current="location">Daily update</a>
+                    </SideNavListItem>
 
-export default withRouter(SideNavMobile);
+                    <SideNavListItem className={`${pathname === '/details/testing' ? "moj-side-navigation__item--active" : ""}`}>
+                        <Link to={ "/details/testing" }
+                              onClick={ () => setMenuState(state => !state) }>Testing</Link>
+                    </SideNavListItem>
+
+                    <SideNavListItem className={`${pathname === '/details/cases' ? "moj-side-navigation__item--active" : ""}`}>
+                        <Link to={ "/details/cases" }
+                              onClick={ () => setMenuState(state => !state) }>Cases</Link>
+                    </SideNavListItem>
+
+                    <SideNavListItem className={`${pathname === '/details/healthcare' ? "moj-side-navigation__item--active" : ""}`}>
+                        <Link to={ "/details/healthcare" }
+                              onClick={ () => setMenuState(state => !state) }>Healthcare</Link>
+                    </SideNavListItem>
+
+                    <SideNavListItem className={`${pathname === '/details/deaths' ? "moj-side-navigation__item--active" : ""}`}>
+                        <Link to={ "/details/deaths" }
+                              onClick={ () => setMenuState(state => !state) }>Deaths</Link>
+                    </SideNavListItem>
+                    <li className={ "moj-side-navigation__item" }>
+                        <hr className={ "govuk-section-break govuk-section-break--s govuk-!-margin-top-0 govuk-!-margin-bottom-0 govuk-section-break--visible" }/>
+                    </li>
+                    <SideNavListItem className={`${pathname === '/details/about-data' ? "moj-side-navigation__item--active" : ""}`}>
+                        <Link to={ "/details/about-data" }
+                        onClick={ () => setMenuState(state => !state) }>About the data</Link>
+                    </SideNavListItem>
+                    <SideNavListItem className={`${pathname === '/details/download' ? "moj-side-navigation__item--active" : ""}`}>
+                        <Link to={ "/details/download" }
+                        onClick={ () => setMenuState(state => !state) }>Download data</Link>
+                    </SideNavListItem>
+                    <SideNavListItem className={`${pathname === '/details/developers-guide' ? "moj-side-navigation__item--active" : ""}`}>
+                        <Link to={ "/details/developers-guide" }
+                        onClick={ () => setMenuState(state => !state) }>Developer's guide</Link>
+                    </SideNavListItem>
+                </SideNavList>
+                <SectionBreak/>
+            </SideNav>
+            : null
+    }</MobileNavWrapper>;
+
+};  // SideNavMobile
+
+
+export default SideNavMobile;
