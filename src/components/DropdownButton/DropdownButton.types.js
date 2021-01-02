a03a5b6db391385d627c3b761e2f5f9d78a5e42c
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-01-02 21:28:08 +0000

    minor changes

--- src/components/DropdownButton/DropdownButton.types.js
+++ src/components/DropdownButton/DropdownButton.types.js
@@ -3,13 +3,13 @@ import type { ComponentType } from "react";
 
 export interface DropdownButtonProps {
 
-    launcherSrOnly: string
-    buttonLabel: string
-    icon: string
-    props: {||}
-    tooltip?: string
-    children?: ComponentType
-    optionsProps?: {||}
+    launcherSrOnly: string,
+    buttonLabel:    string,
+    icon:           string,
+    props:          {||},
+    tooltip?:       string,
+    children?:      ComponentType,
+    optionsProps?:  {||},
     launcherProps?: {||}
 
 }  // DropdownButtonProps
