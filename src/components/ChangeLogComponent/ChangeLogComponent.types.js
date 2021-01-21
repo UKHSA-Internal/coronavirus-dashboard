6a08ecb48a68695060c2ac1008658be4f2b7ff69
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-01-21 11:36:43 +0000

    implements input props type

---
+++ src/components/ChangeLogComponent/ChangeLogComponent.types.js
@@ -0,0 +1,17 @@
+// @flow
+
+import type { ElementProps } from "react";
+
+
+export interface ChangeLogItemType {
+    type: string,
+    date: string,
+    displayBanner?: boolean,
+    relativeUrl?: string,
+    headline: string,
+    body: string
+}
+
+export interface ChangeLogInputProps extends ElementProps {
+    data: ChangeLogItemType
+}
