fc48822c1e7b86345f1be58024aa9ae95efa600b
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-02-04 14:57:59 +0000

    adds + integrates markdown renderer and details section

--- src/components/ChangeLogComponent/ChangeLogItem.js
+++ src/components/ChangeLogComponent/ChangeLogItem.js
@@ -1,21 +1,63 @@
 // @flow
 
-import React from "react";
+import React, { useState, useMemo } from "react";
 import moment from "moment";
 import { Markdown } from "./ChangeLogComponent.styles";
 import { Link } from "react-router-dom";
 
 import type { ComponentType } from "react";
+import remark from "remark";
+import html from "remark-html";
+
+
+const useMarkdown = (content: string | undefined) => {
+
+    const [body, setBody] = useState(null);
+
+    useMemo(() => {
+        remark()
+            .use(html)
+            .process(content, (err, text) => {
+                setBody(err ? null : String(text));
+            });
+
+    }, [ content ])
+
+    return body
+
+};  // ProcessedMarkdown
 
 
 const ChangeLogItemBody: ComponentType = ({ data }) => {
 
+    const body = useMarkdown(data?.body)
+
     return <div className="govuk-body govuk-!-margin-top-0 govuk-!-margin-bottom-0">
         <Markdown className="govuk-body govuk-!-margin-top-0 govuk-!-margin-bottom-0"
-                  dangerouslySetInnerHTML={{ __html: data.body }}/>
+                  dangerouslySetInnerHTML={{ __html: body }}/>
     </div>
 
 }; // ChangeLogItemBody
+
+
+const Details: ComponentType = ({ data }) => {
+
+    const details = useMarkdown(data?.details);
+
+    if ( !details ) return null;
+
+    return <details className="govuk-details govuk-!-margin-top-2"
+                    data-module="govuk-details">
+        <summary className="govuk-details__summary">
+            <span className="govuk-details__summary-text">
+                Additional details
+            </span>
+        </summary>
+        <Markdown className="govuk-details__text govuk-body-s govuk-!-margin-top-0 govuk-!-margin-bottom-0"
+                  dangerouslySetInnerHTML={{ __html: details }}/>
+    </details>
+
+};  // Details
 
 
 const ChangeLogHeading: ComponentType = ({ data }) => {
@@ -48,6 +90,7 @@ export const ChangeLogItem: ComponentType = ({ data, changeTypes, colour }) => {
     return <div className="govuk-body-s govuk-!-margin-top-3 govuk-!-margin-bottom-6">
         <ChangeLogHeading data={ data }/>
         <ChangeLogItemBody changeTypes={ changeTypes } data={ data }/>
+        <Details data={ data }/>
     </div>
 
 }; // ChangeLogItem
