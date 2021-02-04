b3c3edbe4d658449c87aed2f24a74adf11bad032
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-02-04 11:54:56 +0000

    changes implementation to merge links with heading

--- src/components/ChangeLogComponent/ChangeLogItem.js
+++ src/components/ChangeLogComponent/ChangeLogItem.js
@@ -2,10 +2,7 @@
 
 import React from "react";
 import moment from "moment";
-import {
-    // ChangeLogSpan,
-    Markdown
-} from "./ChangeLogComponent.styles";
+import { Markdown } from "./ChangeLogComponent.styles";
 import { Link } from "react-router-dom";
 
 import type { ComponentType } from "react";
@@ -16,30 +13,40 @@ const ChangeLogItemBody: ComponentType = ({ data }) => {
     return <div className="govuk-body govuk-!-margin-top-0 govuk-!-margin-bottom-0">
         <Markdown className="govuk-body govuk-!-margin-top-0 govuk-!-margin-bottom-0"
                   dangerouslySetInnerHTML={{ __html: data.body }}/>
-        <Link to={ data.relativeUrl } className={ "govuk-link govuk-!-font-weight-bold" }>
-            { data.linkText }
-        </Link>
     </div>
 
 }; // ChangeLogItemBody
 
 
+const ChangeLogHeading: ComponentType = ({ data }) => {
+
+    return <h3 className={ "govuk-heading-s govuk-!-font-size-19 govuk-!-margin-bottom-1" }>
+        <small className={ "govuk-caption-m govuk-!-font-size-19 govuk-!-margin-bottom-1" }>
+            <time dateTime={data.date}>
+                <span className={ "govuk-visually-hidden" }>Date of change: </span>
+                { moment(data.date).format("D MMMM") }
+            </time>
+            {/*<ChangeLogSpan color={ colour?.text ?? "#000000" }*/}
+            {/*               bgColor={ colour?.background ?? "inherit" }>*/}
+            {/*    { data.type }*/}
+            {/*</ChangeLogSpan>*/}
+        </small>
+        {
+            data?.relativeUrl
+                ? <Link to={ data.relativeUrl } className={ "govuk-link govuk-!-font-weight-bold" }>
+                    { data.headline }
+                </Link>
+                : data.headline
+        }
+    </h3>
+
+};  // ChangeLogHeading
+
+
 export const ChangeLogItem: ComponentType = ({ data, changeTypes, colour }) => {
 
     return <div className="govuk-body-s govuk-!-margin-top-3 govuk-!-margin-bottom-6">
-        <h3 className={ "govuk-heading-s govuk-!-font-size-19 govuk-!-margin-bottom-1" }>
-            <small className={ "govuk-caption-m govuk-!-font-size-19 govuk-!-margin-bottom-1" }>
-                <time dateTime={data.date}>
-                    <span className={ "govuk-visually-hidden" }>Date of change: </span>
-                    { moment(data.date).format("D MMMM") }
-                </time>
-                {/*<ChangeLogSpan color={ colour?.text ?? "#000000" }*/}
-                {/*               bgColor={ colour?.background ?? "inherit" }>*/}
-                {/*    { data.type }*/}
-                {/*</ChangeLogSpan>*/}
-            </small>
-            { data.headline }
-        </h3>
+        <ChangeLogHeading data={ data }/>
         <ChangeLogItemBody changeTypes={ changeTypes } data={ data }/>
     </div>
 
