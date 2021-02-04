722f8a9765a426ba43f33b0b77c1040163859374
david.urquhart <david.urquhart@phe.gov.uk>
2021-02-04 15:25:40 +0000

    use Detail and Accordian component

--- src/components/MetricAvailability/MetricAvailability.js
+++ src/components/MetricAvailability/MetricAvailability.js
@@ -21,6 +21,8 @@ import {
     Container } 
 from './MetricAvailability.styles';
 
+import URLs from "common/urls";
+
 import searchContent from './MetricTextSearch';
 
 import MetricTextSearch from './MetricTextSearch';
@@ -50,12 +52,14 @@ const MetricHeader = [
     'Wales'
 ];
 
-const Spacer: ComponentType<Props> = () => {
-    return <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" } style={{gridColumn: "1/ span 6"}}/>
-}
+const MetricCard: ComponentType<Props> = ({ item, metric }: Props) => {
 
+    const detailsBaseUrl = URLs["metricDetails"];
+    const descriptionMd = (Object.values(metric)[0])[1].description
+    const methodologyMd = (Object.values(metric)[0])[1].methodology
 
-const MetricCard: ComponentType<Props> = ({ item, metric, expanded }: Props) => {
+    const description = useGenericAPI(detailsBaseUrl + descriptionMd, "Not currently available", "text" );
+    const methodology = useGenericAPI(detailsBaseUrl + methodologyMd, "Not currently available", "text" );
 
     const areaType = [
         'nation',
@@ -65,13 +69,10 @@ const MetricCard: ComponentType<Props> = ({ item, metric, expanded }: Props) => 
         'overview',
     ];
 
-    const card = expanded ? 
- 
-        areaType.map((at, index) => {
+    const card = areaType.map((at, index) => {
 
         const availability = (Object.values(metric)[0])[1].availability
-        console.log(availability)
-     
+       
         return <>
             {/* Area Name  */}
             <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" } key={ `metric-an-${ index }`} style={{height: '2px', borderTop: "1px solid #e5e5e5"}}>
@@ -80,110 +81,150 @@ const MetricCard: ComponentType<Props> = ({ item, metric, expanded }: Props) => 
             {/* UK */}
             <div className={ "govuk-!-margin-top-20 govuk-!-margin-bottom-0" }key={ `metric-uk-${ index }`} style={{height: '2px', borderTop: "1px solid #e5e5e5"}}>
                 <div className={ "govuk-!-margin-top-1 govuk-!-margin-bottom-1" }>
-                    { availability.includes("K") ? <img src={ GreenTick } width={ "14px" } /> : null}
+                    { availability[at] ? availability[at].includes("K") ? <img src={ GreenTick } width={ "14px" } /> : null : null}
                 </div>
             </div>
             {/* England */}
-            {/* <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" } key={ `metric-en-${ index }`} style={{height: '2px', borderTop: "1px solid #e5e5e5"}}>
+            <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" } key={ `metric-en-${ index }`} style={{height: '2px', borderTop: "1px solid #e5e5e5"}}>
                 <div className={ "govuk-!-margin-top-1 govuk-!-margin-bottom-1" }>
-                    { availability.includes("E") ? <img src={ GreenTick } width={ "14px" } /> : null}
+                    { availability[at] ? availability[at].includes("E") ? <img src={ GreenTick } width={ "14px" } /> : null : null}
                 </div>
-            </div> */}
+            </div>
             {/* Scotland */}
-            {/* <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" }key={ `metric-sc-${ index }`} style={{height: '2px', borderTop: "1px solid #e5e5e5"}}>
+            <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" }key={ `metric-sc-${ index }`} style={{height: '2px', borderTop: "1px solid #e5e5e5"}}>
                 <div className={ "govuk-!-margin-top-1 govuk-!-margin-bottom-1" }>
-                    { availability.includes("S") ? <img src={ GreenTick } width={ "14px" } /> : null}
+                    { availability[at] ? availability[at].includes("S") ? <img src={ GreenTick } width={ "14px" } /> : null : null}
                 </div>
-            </div> */}
+            </div>
             {/* NI */}
-            {/* <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" } key={ `metric-ni-${ index }`} style={{height: '2px', borderTop: "1px solid #e5e5e5"}}>
+            <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" } key={ `metric-ni-${ index }`} style={{height: '2px', borderTop: "1px solid #e5e5e5"}}>
                 <div className={ "govuk-!-margin-top-1 govuk-!-margin-bottom-1" }>
-                    { availability.includes("N") ? <img src={ GreenTick } width={ "14px" } /> : null}
+                    { availability[at] ? availability[at].includes("N") ? <img src={ GreenTick } width={ "14px" } /> : null: null}
                 </div>
-            </div> */}
+            </div>
             {/* Wales */}
-            {/* <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" } key={ `metric-wa-${ index }`} style={{height: '2px', borderTop: "1px solid #e5e5e5"}}>
+            <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" } key={ `metric-wa-${ index }`} style={{height: '2px', borderTop: "1px solid #e5e5e5"}}>
                 <div className={ "govuk-!-margin-top-1 govuk-!-margin-bottom-1" }>
-                    { availability.includes("W") ? <img src={ GreenTick } width={ "14px" } /> : null}
+                    { availability[at] ? availability[at].includes("W") ? <img src={ GreenTick } width={ "14px" } /> : null : null}
                 </div>
-            </div> */}
+            </div>
+  
             </>
 
-    }) : null;
+    });
 
-    return <>   
+   
+
+    const metricDescription =   
+            <div style={{gridColumn: "1/ span 6"}}>
+                <details class="govuk-details" data-module="govuk-details">
+                <summary class="govuk-details__summary">
+                    <span class="govuk-details__summary-text">
+                        Metric description
+                    </span>
+                </summary>
+                <div class="govuk-details__text">
+                    {description}
+                </div>
+            </details>
+            </div>;
+
+  
+    const metricMethodology =    
+        <div style={{gridColumn: "1/ span 6"}}>
+            <details class="govuk-details" data-module="govuk-details">
+                <summary class="govuk-details__summary">
+                    <span class="govuk-details__summary-text">
+                        Methodology
+                    </span>
+                </summary>
+                <div class="govuk-details__text">
+                    {methodology}
+                </div>
+            </details> 
+        </div>;
+
+    return <> 
         {card}
+        {metricMethodology}
+        {metricDescription}
+       
     </>
 
 };
 
-const MetricItem: ComponentType<Props> = ({ item, metric, expandAll, callClose }: Props) => {
-
-    console.log("MetricItem")
+const MetricItem: ComponentType<Props> = ({ item, metric, allExpanded }: Props) => {
 
     const dateAdded = (Object.values(metric)[0])[1].dateAdded
 
     const DATE_FORMAT = "DD/MM/YYYY";
 
-    const [ cardOpen, setCardOpen ] = useState(false)
+    const [ expanded, setExpanded ] = useState(false)
+    const [ cls, setCls] = useState ("govuk-accordion__section")
 
-    const closeOrExpandCard = () => {
-        setCardOpen(!cardOpen)
-        callClose(!cardOpen, item)
+    const expandSection = () => {
+        if (expanded === true) allExpanded = false;
+        setExpanded(!expanded)
     }
 
     useEffect(() => {
-        setCardOpen(expandAll);
-    }, [ expandAll ]);
+        if (expanded || allExpanded) {
+            setCls("govuk-accordion__section govuk-accordion__section--expanded")
+        }
+        else {
+            setCls("govuk-accordion__section")
+        }
+    }, [ expanded, allExpanded ]);
 
     return <>
 
-    {/* <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" } style={{gridColumn: "1/ span 5",  borderTop: "2px solid #e5e5e5"}}>
-        <p id={ "metric-description" } className={ "govuk-heading-s" }>
-            {metric[item]["description"]}
-        </p> 
-    </div> */}
+      
 
-    <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" } style={{gridColumn: "6/ span 1", borderTop: "2px solid #e5e5e5"}}>
-        <button className={ "govuk-button govuk-!-margin-top-2 govuk-!-margin-bottom-0" } onClick={closeOrExpandCard}>
-            {cardOpen ? "-" : "+"}
-        </button>
-    </div>
+      <div key={ `metric-section-${ item }`} class={cls}>
+            <div class="govuk-accordion__section-header">
+                <h2 class="govuk-accordion__section-heading">
 
-    <div className={"govuk-!-margin-top-0 govuk-!-margin-bottom-0"} style={{gridColumn: "1/ span 6"}}>
-        <p id={ "metric-name" } className={ "govuk-heading-s" }>
-            Metric name: {item} Date added: {moment(dateAdded).format(DATE_FORMAT)}
-        </p>
-    </div>
+                    <button type="button" onClick={expandSection} id={ `accordion-default-heading-${ item }`} aria-controls={ `accordion-default-content-${ item }`} class="govuk-accordion__section-button" aria-expanded={expanded}>
 
-    
+                        <div className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" }>
+                            {/* <p id={ "metric-????" } className={ "govuk-heading-s" }>
+                                {???}
+                            </p>  */}
+                        </div>
 
-    <MetricDataHeader header={ MetricHeader }
-                      expanded={cardOpen}/>
+                        <div className={"govuk-!-margin-top-0 govuk-!-margin-bottom-0"}>
+                            <p id={ "metric-name" } className={ "govuk-heading-s" }>
+                                Metric name: {item} Date added: {moment(dateAdded).format(DATE_FORMAT)}
+                            </p>
+                       </div>
+                        
+                       <span class="govuk-accordion__icon" aria-hidden="true"></span>
+                    </button>
+                </h2>
+            </div>
+            <div id={ `accordion-default-content-${ item }`} class="govuk-accordion__section-content" aria-labelledby={ `accordion-default-heading-${ item }`}>
+                <Container>
+                    <MetricDataHeader header={ MetricHeader }/>
 
-    <MetricCard item={ item }
-                metric={ metric }
-                expanded={cardOpen}/>
-
-    <Spacer/>
+                    <MetricCard item={ item }
+                                metric={ metric }/>
+                </Container>
+            </div>
+        </div>
 
    
-  </>
+    </>
 } // MetricItem
 
-const MetricDataHeader: ComponentType<Props> = ({ header, expanded }: Props) => {
+const MetricDataHeader: ComponentType<Props> = ({ header }: Props) => {
 
-    if (expanded) {
-        return header.map((item, index) => {
+   
+    return header.map((item, index) => {
                     return <div key={ `metric-header-${ index }`} 
                             style={{height: '2px', gridColumn:  "{index+1}/ span 1"}} 
                             className={ "govuk-!-margin-top-0 govuk-!-margin-bottom-0" }>{item}</div> 
                 });  
-    }
-    else {
-        return null;
-    
-    }       
+       
 } // MetricDataHeadrer
 
 
@@ -205,18 +246,9 @@ const MetricAvailabilty: ComponentType<Props> = ({ data }: Props) => {
         'Count'
     ];
 
+    console.log("MA")
+    console.log(data)
     const metrics = Object.keys(data[Object.keys(data)[1]])
-
-    const [closeAll, setCloseAll] = useState([]);
-
-    useEffect(() => {
-
-        const m = metrics.map(item => {
-            return {[item]: false} 
-        });
-        
-        setCloseAll(m)
-    }, [ ]);
 
     const topics = new Set(Object.values(data[Object.keys(data)[0]]["categories"]))
     const typs = new Set(Object.values(data[Object.keys(data)[0]]["types"]))
@@ -224,34 +256,11 @@ const MetricAvailabilty: ComponentType<Props> = ({ data }: Props) => {
     const [topicType, setTopicType] = useState(null);
     const [typeType, setTypeType] = useState(null);
 
-    const [expandAll, setExpandAll] = useState(false);
+    const [allExpanded, setAllExpanded ] = useState(false);
  
-
-    const closeOrExpandAll = () => {
-        setExpandAll(!expandAll);
+    const setAllExp = () => {
+        setAllExpanded(!allExpanded)
     }
-
-    const callClose = (close, metric) => {  
-        
-        if (close) {
-            const m = metrics.map(item => {
-            
-                if (metric === item) {
-                    return {[item]: close} 
-                } else {
-                    return {[item]: false}
-                }
-                
-            });
-            setCloseAll(m)
-        }
-    };
-
-    useEffect(() => {
-        // const allOpen = Object.values(closeAll)[0].some((key) => closeAll[key] === false);
-        const allOpen = false;
-        if (allOpen) setExpandAll(true)
-    }, [ closeAll ]);
 
     return <Fragment>
 
@@ -259,8 +268,7 @@ const MetricAvailabilty: ComponentType<Props> = ({ data }: Props) => {
 
         <MainContainer>
             <MainContent className={ "no-border" }>
-
-                  
+  
                 <div className={ "govuk-phase-banner status-banner govuk-!-margin-bottom-0" }>
                     <p className={ "govuk-phase-banner__content" }>
                         This page lists all the available metrics, displaying their availability across
@@ -268,31 +276,29 @@ const MetricAvailabilty: ComponentType<Props> = ({ data }: Props) => {
                     </p>
                 </div>
 
-                <Container> 
-                    <div style={{gridColumn: "6/ span 1"}}>
-                        <button className={ "govuk-button govuk-!-margin-top-0 govuk-!-margin-bottom-0" }
-                                onClick={closeOrExpandAll}>
-                            {expandAll ? "Close all" : "Open all"}
-                        </button>
-                    </div>
-                </Container>
+                <div class="govuk-accordion" data-module="govuk-accordion" id="accordion-default">
 
-                <Container>     
+                <div class="govuk-accordion__controls">
+                    <button onClick={setAllExp}type="button" class="govuk-accordion__open-all" aria-expanded={allExpanded}>{allExpanded ? "Close all": "Open all"}
+                        <span class="govuk-visually-hidden"> sections</span>
+                    </button>
+                </div>
                     {
-        
+            
                         metrics.map(item => {
-                           
+                            
                             const metric = Object.entries(Object.entries(data)[1][1]).filter(it => it[0] === item)
-                        
+                            
                             return <MetricItem 
                                     item={ item }
                                     metric={ metric }
-                                    expandAll={ expandAll }
-                                    callClose={ callClose }/>
+                                    allExpanded={ allExpanded }/>
 
                         })
                     }
-                </Container>
+                  
+                </div>
+
 
             </MainContent>
 
--- src/pages/Metrics/Metrics.js
+++ src/pages/Metrics/Metrics.js
@@ -9,48 +9,9 @@ import MetricAvailability from "components/MetricAvailability";
 
 const Metrics = () => {
 
-    // const data = useGenericAPI("metricsAvailability", [], "json");
+    const data = useGenericAPI("metricsAvailability", null, "json");
 
-    const data = {"References":{"categories":{"cases":"cases","testing":"testing","vaccination":"vaccination","deaths":"deaths","healthcare":"healthcare","other":"other"},"types":{"cumulative":"cumulative","daily":"daily","demographics":"demographics","eventDate":"eventDate","nso":"nso","rate":"rate","reportingDate":"reportingDate","rollingRate":"rollingRate","weekly":"weekly"}},"metrics":{"alertLevel":{"PUBLISHED":true,"abstract":null,"availability":{"ltla":"ES"},"dateAdded":"2021-02-03","dateDeprecated":null,"description":"alertLevel.md","methodology":null,"sources":{"England":null,"Scotland":null},"tags":["other"],"updateFrequency":null},"capacityPillarFour":{"PUBLISHED":true,"abstract":null,"availability":{"overview":"K"},"dateAdded":"2021-02-02","dateDeprecated":null,"description":"capacityPillarFour.md","methodology":null,"sources":{"United Kingdom":null},"tags":["testing"],"updateFrequency":null}}}
-
-    // const data = {
-    //     metrics: [
-    //         {
-    //             'newCasesBySpecimenDate': {
-    //                 'overview': 'K',
-    //                 'nation': 'ENSW',
-    //                 'region': 'E',
-    //                 'utla': 'ENSW',
-    //                 'ltla': 'ENSW',
-    //                 'msoa': 'E',
-    //                 'tag': ['cases', 'eventDate'],
-    //                 'description': 'New Cases by Specimen date',
-    //                 'methodology': '1 paragraph',
-    //                 'abstract': 'Abstract',
-    //                 'dateAdded': '2021-01-21',
-    //                 'dateDeprecated': ''
-    //             }
-    //         },
-    //         {
-    //             'cumCasesBySpecimenDate': {
-    //                 'overview': 'K',
-    //                 'nation': 'ENSW',
-    //                 'region': 'E',
-    //                 'utla': 'ENSW',
-    //                 'ltla': 'ENSW',
-    //                 'msoa': 'E',
-    //                 'tag': ['cases', 'eventDate'],
-    //                 'description': 'Cumulative Cases by Specimen date',
-    //                 'methodology': '1 paragraph',
-    //                 'abstract': 'Abstract',
-    //                 'dateAdded': '2021-01-22',
-    //                 'dateDeprecated': ''
-    //             }
-    //         },
-    //     ]
-    // }
-
-    // if ( !data ) return <Loading/>;
+    if ( !data ) return <Loading/>;
 
     return <MetricAvailability data={ data }/>
 
