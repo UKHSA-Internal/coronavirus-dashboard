fdfac96b72e26d4961641fa7744784d43799821c
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-02-08 11:34:46 +0000

    adds anchor element and time component

--- src/components/ChangeLogComponent/ChangeLogHeader.js
+++ src/components/ChangeLogComponent/ChangeLogHeader.js
@@ -3,7 +3,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
-
+import { heading2id } from "common/utils";
 import { ChangeLogBanner } from "./ChangeLogComponent.styles";
 
 import type { ChangeLogInputProps } from "./ChangeLogComponent.types";
@@ -19,8 +19,10 @@ export const ChangeLogHeader: ComponentType<*> = ({ data, ...props }: ChangeLogI
     return <>{
         data.map((item, index) => <ChangeLogBanner key={ `change-log-banner-${index}` }>
             <div className={ "govuk-body-s govuk-!-font-weight-bold govuk-!-margin-bottom-0" }>
-                { moment(item.date).format("D MMM YYYY") } &mdash; { item.headline }
-                <Link to={ "/details/whats-new" }
+                <time dateTime={ item.date }>
+                    { moment(item.date).format("D MMMM YYYY") }
+                </time> &mdash; { item.headline }
+                <Link to={ `/details/whats-new#${ heading2id(item.headline) }` }
                       className={ "govuk-link govuk-link--no-visited-state govuk-!-margin-left-1" }>
                     More
                 </Link>
