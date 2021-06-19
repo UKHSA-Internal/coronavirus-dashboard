4a480c94c4ad7132c5f8847fa224ed9d5b480360
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-19 13:24:49 +0100

    includes map

--- src/components/SideNavigation/SideNavigation.js
+++ src/components/SideNavigation/SideNavigation.js
@@ -51,6 +51,9 @@ const SideNavigation: ComponentType<Props> = ({ ...props }: Props) => {
         <SectionBreak/>
 
         <SideNavSecondaryContainer>
+            <SideNavListSecondaryItem className={`${pathname === '/details/interactive-map' ? "moj-side-navigation__item--active" : ""}`}>
+                <Link to={ "/details/interactive-map" }>Interactive map</Link>
+            </SideNavListSecondaryItem>
             <SideNavListSecondaryItem className={`${pathname === '/details/about-data' ? "moj-side-navigation__item--active" : ""}`}>
                 <Link to={ "/details/about-data" }>About the data</Link>
             </SideNavListSecondaryItem>
