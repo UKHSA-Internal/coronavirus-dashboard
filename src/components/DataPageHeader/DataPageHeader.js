42bd3bca064b4a2ee8d7a049f3a81ce8a5d76a34
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-07-21 15:01:18 +0100

    integrates conditional page headers into metadata generator

--- src/components/DataPageHeader/DataPageHeader.js
+++ src/components/DataPageHeader/DataPageHeader.js
@@ -1,13 +1,13 @@
 // @flow
 
-import React, { useEffect } from "react";
+import React from "react";
 
-import { analytics, getParams, getParamValueFor } from "common/utils";
+import { getParams, getParamValueFor } from "common/utils";
 
 import { Helmet } from "react-helmet";
+import { useLocation } from "react-router";
 
 import Path from "assets/paths.json";
-
 
 const DefaultParams = [
     { key: 'areaName', sign: '=', value: 'United Kingdom' },
@@ -15,45 +15,38 @@ const DefaultParams = [
 ];
 
 
-const DataPageHeaders = ({ pathname, query }) => {
+const DataPageHeaders = () => {
 
-    const { title="", description="", localised=false } = Path?.[pathname] ?? {};
+    const { search: query, pathname } = useLocation();
+    const uri = /^(\/(details\/)?[^/]+).*/.exec(pathname)?.[1];
+    let { title="", description="", localised=false } = Path?.[uri] ?? {};
     const urlParams = getParams(query);
     const areaParams = urlParams.length ? urlParams : DefaultParams;
 
-    let areaName = getParamValueFor(areaParams, "areaName");
+    let areaName;
 
-    if ( areaName.toLowerCase() === "united kingdom" ) {
-        areaName = "the UK";
+    if ( localised ) {
+        areaName = getParamValueFor(areaParams, "areaName");
+
+        if ( areaName.toLowerCase() === "united kingdom" ) {
+            areaName = "the UK";
+        }
     }
 
     let [preppedTitle, preppedDescription] = localised
         ? [`${title} in ${areaName}`, `${description} in ${ areaName }`]
         : [title, description];
 
-    useEffect(() => {
-        if ( Array.isArray(window?.dataLayer) ) {
-            analytics({
-                action: "page_view",
-                page_title: preppedTitle,
-                page_location: document.location.href,
-                page_path: pathname,
-                send_to: 'UA-161400643-2'
-            });
-        }
-    }, [ pathname, query, preppedTitle ]);
-
-    preppedTitle =  decodeURIComponent(`${preppedTitle} | Coronavirus in the UK`);
-    preppedDescription = decodeURIComponent(preppedDescription);
+    preppedTitle +=  ' | Coronavirus in the UK';
 
     return <Helmet>
-        <title>{ preppedTitle }</title>
-        <meta name="description" content={ preppedDescription } />
+        <title itemProp={ "name" }>{ preppedTitle }</title>
+        <meta name="description" itemProp={ "abstract" } content={ preppedDescription } />
         <meta property="og:title" content={ preppedTitle }/>
         <meta name="twitter:title" content={ preppedTitle }/>
         <meta property="og:description" content={ preppedDescription }/>
         <meta name="twitter:description" content={ preppedDescription }/>
-    </Helmet>;
+    </Helmet>
 
 };
 
