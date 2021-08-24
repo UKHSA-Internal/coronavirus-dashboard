58a2f58b05f3d29b9ccaaa6766e85d6dab56c616
Brogan <66849242+grobaan@users.noreply.github.com>
2021-08-24 10:56:58 +0100

    replaces class with function

--- src/pages/GenericApiDocs/GenericApiDocs.js
+++ src/pages/GenericApiDocs/GenericApiDocs.js
@@ -2,16 +2,10 @@ import React, { Component } from 'react';
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
-import type { GenericApiDocsProps } from './GenericApiDocs.types';
-        
-export default class GenericApiDocs extends Component<GenericApiDocsProps, {}> {
-
-    render(): React$Node {
-
-        return <>
+function GenericApiDocs() {
+    return <>
             <SwaggerUI url="https://raw.githubusercontent.com/publichealthengland/coronavirus-dashboard-generic-apis/development/assets/openapi.json" />
-        </>
+        </>   
+};
 
-    }
-
-} 
+export default GenericApiDocs;
--- src/pages/GenericApiDocs/GenericApiDocs.types.js
+++ src/pages/GenericApiDocs/GenericApiDocs.types.js
@@ -1,7 +1,2 @@
 // @flow
 
-export interface GenericApiDocsProps {
-
-    value: string
-
-}
