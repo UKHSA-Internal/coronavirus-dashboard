0fa87dc70e3ac761aec62fe95d8f75242b26d130
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-11-17 11:58:51 +0000

    implements conditional addition of TOC

--- src/hooks/useMarkdown.js
+++ src/hooks/useMarkdown.js
@@ -4,10 +4,11 @@ import React, { useMemo, useState } from "react";
 
 import remark from "remark";
 import html from "remark-html";
+import toc from "remark-toc";
 import slug from "remark-slug";
 import externalLink from "remark-external-links";
 
-export const useMarkdown = (content: string | undefined) => {
+export const useMarkdown = (content: string | undefined, withToc=false) => {
 
     const [body, setBody] = useState(null);
 
@@ -15,9 +16,10 @@ export const useMarkdown = (content: string | undefined) => {
         remark()
             .use(slug)
             .use(externalLink)
+            .use(toc)
             .use(html)
             .process(content, (err, text) => {
-                setBody(err ? null : String(text));
+                setBody(err ? null : String(text).replace(/(id=")user-content-/ig, '$1'));
             });
 
     }, [ content ])
