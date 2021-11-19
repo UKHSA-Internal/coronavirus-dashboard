9c8cdd70b551b66f89f96c77df2998edeb9a7359
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-11-19 14:28:22 +0000

    improves styling on tablets / mobiles

--- src/pages/MetricDocs/Documentation/Documentation.style.js
+++ src/pages/MetricDocs/Documentation/Documentation.style.js
@@ -28,13 +28,14 @@ export const MarkdownContent: ComponentType<*> =
         `;
 
 
-export const APIMetricContainer: ComponentType<*> =
+export const MainLabelsContainer: ComponentType<*> =
     styled
         .div`
         display: flex;
         flex-wrap: wrap;
         margin-right: auto;
         width: auto;
+        row-gap: .5rem;
         ${ css`${ ({ small }) => ({ 
             ...small 
                 ? { marginTop: '0.2rem', marginBottom: '0.2rem' }
@@ -53,7 +54,6 @@ export const Deprecated: ComponentType<*> =
         align-content: center;
         color: #942514;
         background: #f6d7d2;
-        margin-bottom: .5rem;
         ${ css`${ ({ small }) => ({ 
             ...small 
                 ? { padding: '.25rem .5rem' }
@@ -69,9 +69,9 @@ export const APILabel: ComponentType<*> =
             className: `govuk-!-font-size-${ small ? 14 : 16 } ${className}`,
             ...props
         }))`
+        display: flex;
         background: #4c4c4c; 
         color: white;
-        margin-bottom: .5rem;
         ${ css`${ ({ small }) => ({ 
             ...small 
                 ? { padding: '.25rem .5rem' }
@@ -90,12 +90,27 @@ export const APIMetric: ComponentType<*> =
         background: #d9d9d9;
         font-family: menlo, monaco, courier, monospace;
         margin-right: .5rem;
-        margin-bottom: .5rem;
+        word-break: break-all;
+                
         ${ css`${ ({ small }) => ({ 
             ...small 
                 ? { padding: '.25rem .5rem' }
                 : { padding: '.5rem 1rem' }
         })}`}
+        `;
+
+
+export const APIMetricContainer: ComponentType<*> =
+    styled
+        .span
+        .attrs(({ small, className="", ...props }) => ({
+            className: `govuk-!-font-size-${ small ? 14 : 16 } ${className}`,
+            ...props
+        }))`
+        display: flex;
+        flex-flow: row;
+        flex-wrap: wrap;
+        row-gap: 0;
         `;
 
 
@@ -108,17 +123,38 @@ export const MetadataContainer: ComponentType<*> =
         }))`
         display: grid;
         grid-template-columns: auto 1fr;
+        row-gap: .5rem;
+        
+        & .light-blue {
+            color: #144e81;
+            background: #d2e2f1;
+        }
+        
+        & .dark-blue {
+            color: #fff;
+            background: #1d70b8;
+        }
+        
+        & .light-blue,
+        & .dark-blue {
+            padding: .2rem .4rem;
+            font-size: 14px;
+            font-weight: bold;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+        }
         
         & > dt {
-            grid-column: 1;
             font-weight: bold;
+            max-width: fit-content;
+            justify-self: end;
         }
         
         & > dd {
-            grid-column: 2;
             display: flex;
             flex-direction: column;
-            row-gap: .3rem;
+            row-gap: .3rem;         
+            max-width: none;   
             
             & > * {
                 white-space: nowrap;
