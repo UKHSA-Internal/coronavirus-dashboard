33c983ba935d0642f7e427adcd84d2287b552099
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-11-24 13:18:22 +0000

    implements unified documentation for metrics

--- src/components/Metadata/Metadata.js
+++ src/components/Metadata/Metadata.js
@@ -2,23 +2,64 @@
 
 import React from "react";
 
-import useModalData from "hooks/useModalData";
+import { Link } from "react-router-dom";
+
+import useGenericAPI from "hooks/useGenericAPI";
 import Loading from "components/Loading";
-import { strFormat } from "common/utils";
-import { Markdown } from "./Metadata.styles";
+import Timestamp from "components/Timestamp";
+
+import { MetadataContainer, MetricContainer, MetricsList } from "./Metadata.styles";
 
 import type { ComponentType } from "react";
 
 
-const Metadata: ComponentType<*> = ({ filename, ...props }) => {
+const MetricItem: ComponentType<*> = ({ metric }) => {
 
-    const data = useModalData(filename)
+    const data = useGenericAPI(
+        "genericApiMetricSearch",
+        [],
+        {},
+        "json",
+        { search: metric, exact: "1" }
+    );
 
-    if ( !data ) return <Loading/>;
+    if ( !data?.length ) return <Loading/>;
 
-    return  <Markdown dangerouslySetInnerHTML={{
-        __html: strFormat(data, { kwargs: { date: "latest date available" }})
-    }}/>
+    return data.map(metricData =>
+        <MetricContainer key={ metricData.metric }>
+            <Link className={ "govuk-link govuk-link--no-visited-state" }
+                  to={ `/metrics/doc/${ metricData.metric }` }>
+                { metricData.metric_name }
+            </Link>
+            <span className={ "last-modified govuk-body-s" }>
+                Documentation last updated on: <Timestamp timestamp={ metricData.doc_last_modified }/>
+            </span>
+        </MetricContainer>
+    );
+
+};  // MetricItem
+
+
+const Metadata: ComponentType<*> = ({ download, ...props }) => {
+
+    return <MetadataContainer { ...props }>
+        <p>
+            Documentation for all metrics are available under
+            the <Link className={ "govuk-link govuk-link--no-visited-state" }
+                      to={ "/metrics" }>Metrics documentation</Link> page.
+        </p>
+        <p>
+            Click on each of the { download.length } metrics used in
+            this card to see the details:
+        </p>
+        <MetricsList>{
+            download.map(metric =>
+                <li key={ metric }>
+                    <MetricItem metric={ metric }/>
+                </li>
+            )
+        }</MetricsList>
+    </MetadataContainer>;
 
 };  // Metadata
 
--- src/components/Metadata/Metadata.styles.js
+++ src/components/Metadata/Metadata.styles.js
@@ -5,9 +5,8 @@ import styled from "styled-components";
 import type { ComponentType } from "react";
 
 
-export const Markdown: ComponentType<*> = (() => {
-
-    return styled
+export const Markdown: ComponentType<*> =
+    styled
         .div
         .attrs(({ className="" }) => ({
             className: `${ className } markdown modal`
@@ -27,7 +26,37 @@ export const Markdown: ComponentType<*> = (() => {
             & > :first-child {
                 margin-top: 0;
             }            
-        `
+        `;
 
-})();
 
+export const MetricContainer: ComponentType<*> =
+    styled
+        .div`
+            display: flex;
+            flex-direction: column;    
+            
+            & > .last-modified {
+                color: #6B7276;
+                margin-bottom: 1rem;
+            }     
+        `;
+
+
+export const MetadataContainer: ComponentType<*> =
+    styled
+        .div
+        .attrs(({ className="" }) => ({
+            className: `${className} govuk-body govuk-!-padding-right-2`
+        }))`
+            overflow-y: auto;  
+        `;
+
+
+export const MetricsList: ComponentType<*> =
+    styled
+        .ul
+        .attrs(({ className="" }) => ({
+            className: `${className} govuk-list govuk-list--dashed`
+        }))`
+            max-width: 30em;
+        `;
