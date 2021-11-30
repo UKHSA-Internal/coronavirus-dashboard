4476b2cbf3877d0d39b1f216f68f2d456839b519
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-11-30 14:55:54 +0000

    implements styled component for content

--- src/pages/WhatsNew/WhatsNew.styles.js
+++ src/pages/WhatsNew/WhatsNew.styles.js
@@ -1,4 +1,20 @@
 // @flow
+import type { ComponentType } from "react";
+import styled from "styled-components";
 
 
-
+export const Content: ComponentType<*> =
+    styled
+        .section
+        .attrs(({ className="" }) => ({
+            className: `govuk-body-s govuk-!-margin-top-3 govuk-!-margin-bottom-6 ${className}`
+        }))`   
+            max-width: 50em;
+            margin-left: 0; 
+            display: flex !important;
+            flex-direction: column;
+            
+            & * {
+                margin-left: 0 !important;
+            }
+        `;
