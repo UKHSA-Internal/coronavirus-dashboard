f09f246016ff485f9cf2177025b870459324af8c
Jaroslaw Michalski <jaroslaw.m.michalski@phe.gov.uk>
2023-10-02 18:06:45 +0100

    add code to hide/show selected data
    Headline numbers are filteres by the extra attributes.
    If they math areaName and areaType then the data is shown,
    otherwise it's hidden, unless there's no areaName,
    then it's also shown (to keep compatibility with older layout files).

--- src/components/HeadlineNumbers/HeadlineNumbers.js
+++ src/components/HeadlineNumbers/HeadlineNumbers.js
@@ -2,6 +2,8 @@
 
 import React from "react";
 
+import { getParamValueFor } from "common/utils";
+
 import type { Props } from "./HeadlineNumbers.types";
 import ValueBox from "components/ValueBox";
 
@@ -12,18 +14,40 @@ import type { ComponentType } from "react";
 
 const HeadlineNumbers: ComponentType<Props> = ({ params, headlineNumbers=[] }) => {
 
+    const
+        areaType = getParamValueFor(
+            params,
+            "areaType",
+            "overview"
+        ).toLowerCase(),
+        areaName = getParamValueFor(
+            params,
+            "areaName",
+            "United Kingdom"
+        ).toLowerCase();
+
+    const getValueBox = (item, index) => {
+        return <>
+            {
+                (item?.areaName == null || (item?.areaName.toLowerCase() === areaName && item?.areaType.toLowerCase() === areaType))
+                    ? <ValueBox params={ params }
+                                key={ `headline-number-${ index }` }
+                                heading={ item.caption }
+                                { ...item }/>
+                    : null
+            }
+        </>
+    }
+
+    console.log("headline numbers:", headlineNumbers)
+
     return <Container aria-label={ "Headline numbers" }
                       aria-describedby={ 'headline-nums-desc' }>
         <span id={ 'headline-nums-desc' } className={ "govuk-visually-hidden" }>
             Latest available data
         </span>
         {
-            headlineNumbers?.map((item, index) =>
-                <ValueBox params={ params }
-                          key={ `headline-number-${ index }` }
-                          heading={ item.caption }
-                          { ...item }/>
-            ) ?? null
+            headlineNumbers?.map((item, index) => getValueBox(item, index)) ?? null
         }
     </Container>
 
